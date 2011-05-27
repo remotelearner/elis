4537f7be2125ebb55bf92b8ce9a5859408bb8367
Marko Vidberg <marko.vidberg@remote-learner.net>
2011-05-27 16:12:20 -0400

    ELIS-2038 initial migration of course page

--- elis/program/lang/en/elis_program.php
+++ elis/program/lang/en/elis_program.php
@@ -24,24 +24,72 @@
  *
  */
 
+$string['add_course'] = 'Add Course';
 $string['add_user'] = 'Add user';
+$string['adding_completion_element'] = 'Adding Completion Element';
 $string['address'] = 'Address';
 $string['address2'] = 'Address 2';
 $string['admin_dashboard'] = 'Administrator Dashboard';
 
+$string['browse'] = 'Browse';
+
+$string['clear'] = 'Clear';
+$string['completionform:completion_grade_help'] = '<p>Minimum grade the learner must received to identify the element as &ldquo;completed&rdquo;.</p>';
+$string['completionform:course_idnumber_help'] = '<p>When an element is an activity within Moodle, this number should correspond to the id number of that activity in Moodle.</p>';
+$string['completionform:course_name_help'] = '<p>Element name.  Should correspond with the name of the element in Moodle, when Moodle is being used.</p>';
+$string['completionform:course_syllabus_help'] = '<p>Description information about the element.</p>';
+$string['completionform:required'] = '<p>Is the element a required to complete the course? Some elements may be optional and therefore not required for course completion.</p>';
+$string['completion_description'] = 'Description';
+$string['completion_elements'] = 'Completion Elements';
+$string['completion_grade'] = 'Completion grade';
+$string['completion_idnumber'] = 'ID Number';
+$string['completion_name'] = 'Name';
+$string['completion_status'] = 'Completion Status';
+$string['confirm_delete_completion'] = 'Are you sure you want to delete the completion element \"name: $a\"?';
+$string['confirm_delete_course'] = 'Are you sure you want to delete the course named {$a->name} (ID number: {$a->idnumber})?';
 $string['confirm_delete_user'] = 'Are you sure you want to delete the user named {$a->firstname} {$a->lastname} (ID number: {$a->idnumber})?';
+$string['cost'] = 'Cost';
+$string['courseform:completion_grade_help'] = '<p>Minimum grade to complete/pass the course.</p>';
+$string['courseform:cost_help'] = '<p>Registration fee (if any).</p>';
+$string['courseform:course_idnumber_help'] = '<p>Course ID number; may be the same as the code, may be something compoletely different. This number can contain numbers, letters, spaces and special characters and will show up on reports.</p>';
+$string['courseform:course_name_help'] = '<p>Name of course. A course may have many &ldquo;classes&rdquo; (or sometimes called sections). This is the name of the parent course.</p>';
+$string['courseform:course_syllabus_help'] = '<p>Course description.</p>';
+$string['courseform:course_code_help'] = '<p>Roughly corresponds to the &ldquo;short name&rdquo; in Moodle.</p>';
+$string['courseform:course_version_help'] = '<p>Version of course being used. If you update your courses periodically, the update date could be entered here.</p>';
+$string['courseform:coursetemplate_help'] = '<p>Moodle course template/master course, if applicable. This field is only necessary if master Moodle courses are being used to create new instances of courses. This allows the Moodle course template to automatically be copied as a new course when a new instance of a track is created.</p>';
+$string['courseform:credits_help'] = '<p>Number of credits the course is worth. If credits are not being used, this field can be left blank.</p>';
+$string['courseform:curriculum_help'] = '<p>Select the curriculum the course will be assigned to.  A course can be assigned to more than one curricula by contol click.</p>';
+$string['courseform:duration_help'] = '<p>Number of units the course runs.</p>';
+$string['courseform:environment_help'] = '<p>Where/how is the course delivered. Select from the choices available.  If no enviroments have been entered into the system, go to Curriculum Administration block &gt; Information Elements &gt; Environments to enter environment options, such as online, face-to-face, blended, etc.</p>';
+$string['courseform:length_description_help'] = '<p>Defines the units for duration, such as days, weeks, months, semesters, etc.</p>';
+$string['course_classes'] = 'Classes';
+$string['course_curricula'] = 'Curricula';
+$string['course_code'] = 'Code';
+$string['course_curricula'] = 'Curricula';
+$string['course_idnumber'] = 'ID Number';
+$string['course_name'] = 'Name';
+$string['course_syllabus'] = 'Description';
+$string['course_version'] = 'Version';
+$string['coursetemplate'] = 'Course Template in Moodle';
+$string['credits'] = 'Credits';
+$string['curriculum'] = 'Curriculum';
 
 $string['delete'] = 'Delete';
+$string['delete_label'] = 'Delete';
 $string['delete_user'] = 'Delete user';
+$string['deleting_completion_element'] = 'Deleting Completion Element';
 $string['detail'] = 'Detail';
+$string['duration'] = 'Duration';
 
 $string['edit'] = 'Edit';
-$string['email2'] = 'Email address 2';
+$string['editing_completion_element'] = 'Editing Completion Element';
 $string['elispmversion'] = '<strong>ELIS Program Manager Version:</strong> {$a}';
 $string['elis_doc_class_link'] = '<strong>Documentation for ELIS</strong> &mdash; we have over 200
 pages of documentation for ELIS in our <a href="http://training.remote-learner.net/course/view.php?id=1090">ELIS Support Course</a>.
 You can access this course by logging in as a guest.  If you have problems
 accessing this course, please contact your sales representative.';
+$string['email2'] = 'Email address 2';
+$string['environment'] = 'Environment';
 
 $string['fax'] = 'Fax';
 $string['female'] = 'Female';
@@ -49,14 +97,22 @@ $string['female'] = 'Female';
 $string['health_check_link'] = 'The <a href="{$a}/curriculum/index.php?s=health">ELIS health page</a> may help diagnose potential problems with the site.';
 
 $string['id'] = 'ID';
+$string['idnumber_already_used'] = 'ID Number is already in use';
 $string['id_same_as_user'] = 'Same as username';
 $string['inactive'] = 'Inactive';
 
 $string['learningplan'] = 'Learning Plan';
+$string['length_description'] = 'Length Description';
 
+$string['makecurcourse'] = 'Make a curriculum for this course';
 $string['male'] = 'Male';
+$string['manage_course'] = 'Manage Courses';
 $string['manage_user'] = 'Manage users';
 
+$string['no_completion_elements'] = 'There are no completion elements defined.';
+$string['none'] = 'None';
+$string['notice_course_deleted'] = 'Deleted the course named {$a->name} (ID number: {$a->idnumber})';
+$string['num_course_found'] = '{$a} course(s) found';
 $string['num_user_found'] = '{$a} users found';
 
 $string['o_active'] = 'Only active';
@@ -67,9 +123,11 @@ $string['postalcode'] = 'Postal code';
 $string['progman'] = 'Program Manager';
 
 $string['registered_date'] = 'Registered date';
+$string['required'] = 'Required';
 
 $string['showinactive'] = 'Show inactive';
 
+$string['tags'] = 'Tags';
 $string['timecreated'] = 'Creation time';
 $string['transfercredits'] = 'Transfer credits';
 
@@ -79,3 +137,4 @@ $string['usermi'] = 'Middle initials';
 $string['useridnumber'] = 'ID number';
 $string['useridnumber_help'] = 'An id number is a unique value used to identify you within your organization.
 It also serves as way to tie Curriculum Management users to Moodle users.';
+
