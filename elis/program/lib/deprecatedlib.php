ac6a7ce48b6673b8af2ea61fdd044b8b3950bd9c
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-06-10 13:44:09 -0400

    ELIS-2013,ELIS-2062: Add instructor class, new language strings and add cm_print_date_selector, cm_print_time_selector, cm_choose_from_menu to deprecatedlib.php

--- elis/program/lib/deprecatedlib.php
+++ elis/program/lib/deprecatedlib.php
@@ -126,3 +126,126 @@ function cm_get_crlmuserid($userid) {
     return $DB->get_field_sql($select.$from.$join.$where, array($userid));
 }
 
+/**
+ * Prints form items with the names $day, $month and $year
+ *
+ * @param string $day   fieldname
+ * @param string $month  fieldname
+ * @param string $year  fieldname
+ * @param int $currenttime A default timestamp in GMT
+ * @param boolean $return
+ */
+function cm_print_date_selector($day, $month, $year, $currenttime = 0, $return = false) {
+    if (!$currenttime) {
+        $currenttime = time();
+    }
+
+    $currentdate = usergetdate($currenttime); // was cm_usergetdate
+
+    for ($i=1; $i<=31; $i++) {
+        $days[$i] = $i;
+    }
+    for ($i=1; $i<=12; $i++) {
+        //$months[$i] = cm_userdate(gmmktime(12,0,0,$i,15,2000), "%B");
+        $months[$i] = strftime("%B", gmmktime(12,0,0,$i,15,2000));
+    }
+    for ($i=1970; $i<=2020; $i++) {
+        $years[$i] = $i;
+    }
+    return cm_choose_from_menu($days,   $day,   $currentdate['mday'], '', '', '0', $return)
+           . cm_choose_from_menu($months, $month, $currentdate['mon'],  '', '', '0', $return)
+           . cm_choose_from_menu($years,  $year,  $currentdate['year'], '', '', '0', $return);
+}
+
+
+/**
+ *Prints form items with the names $hour and $minute
+ *
+ * @param string $hour  fieldname
+ * @param string ? $minute  fieldname
+ * @param $currenttime A default timestamp in GMT
+ * @param int $step minute spacing
+ * @param boolean $return
+ */
+function cm_print_time_selector($hour, $minute, $currenttime = 0, $step = 5, $return = false) {
+    if (!$currenttime) {
+        $currenttime = time();
+    }
+    $currentdate = usergetdate($currenttime); // was cm_usergetdate
+    if ($step != 1) {
+        $currentdate['minutes'] = ceil($currentdate['minutes']/$step)*$step;
+    }
+    for ($i=0; $i<=23; $i++) {
+        $hours[$i] = sprintf("%02d",$i);
+    }
+    for ($i=0; $i<=59; $i+=$step) {
+        $minutes[$i] = sprintf("%02d",$i);
+    }
+
+    return cm_choose_from_menu($hours,   $hour,   $currentdate['hours'],   '','','0',$return)
+           . cm_choose_from_menu($minutes, $minute, $currentdate['minutes'], '','','0',$return);
+}
+
+
+/**
+ * Given an array of value, creates a popup menu to be part of a form
+ * $options["value"]["label"]
+ *
+ * @param    type description
+ * @todo Finish documenting this function
+ */
+function cm_choose_from_menu ($options, $name, $selected = '', $nothing = 'choose', $script = '',
+                              $nothingvalue = '0', $return = false, $disabled = false,
+                              $tabindex = 0, $id = '') {
+    if ($nothing == 'choose') {
+        //$nothing = get_string('choose') .'...';
+        $nothing = get_string('choose', 'elis_program');
+    }
+
+    $attributes = ($script) ? 'onchange="'. $script .'"' : '';
+    if ($disabled) {
+        $attributes .= ' disabled="disabled"';
+    }
+
+    if ($tabindex) {
+        $attributes .= ' tabindex="'.$tabindex.'"';
+    }
+
+    if ($id ==='') {
+        $id = 'menu'.$name;
+        // name may contaion [], which would make an invalid id. e.g. numeric question type editing form, assignment quickgrading
+        $id = str_replace('[', '', $id);
+        $id = str_replace(']', '', $id);
+    }
+
+    $output = '<select id="'.$id.'" name="'. $name .'" '. $attributes .'>' . "\n";
+    if ($nothing) {
+        $output .= '   <option value="'. s($nothingvalue) .'"'. "\n";
+        if ($nothingvalue === $selected) {
+            $output .= ' selected="selected"';
+        }
+        $output .= '>'. $nothing .'</option>' . "\n";
+    }
+
+    if (!empty($options)) {
+        foreach ($options as $value => $label) {
+            $output .= '   <option value="'. s($value) .'"';
+            if ((string)$value == (string)$selected) {
+                $output .= ' selected="selected"';
+            }
+            if ($label === '') {
+                $output .= '>'. $value .'</option>' . "\n";
+            } else {
+                $output .= '>'. $label .'</option>' . "\n";
+            }
+        }
+    }
+    $output .= '</select>' . "\n";
+
+    if ($return) {
+        return $output;
+    } else {
+        echo $output;
+    }
+}
+
