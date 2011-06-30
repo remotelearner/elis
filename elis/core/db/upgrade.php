677b38399bb7fc000dcbb05b63f73014b5c8e441
brendan <brendan@brendan-desktop.(none)>
2011-06-30 12:58:42 -0400

    ELIS-2072 Removing legacy steps from upgrade

--- elis/core/db/upgrade.php
+++ elis/core/db/upgrade.php
@@ -30,68 +30,6 @@ function xmldb_elis_core_upgrade($oldversion=0) {
     $dbman = $DB->get_manager();
     $result = true;
 
-    if ($result && $oldversion < 2011030402) {
-        /***********************************************************************
-         * Replace plugintype and pluginname with plugin field
-         **********************************************************************/
-
-    /// Define field plugin to be added to elis_scheduled_tasks
-        $table = new XMLDBTable('elis_scheduled_tasks');
-        $field = new XMLDBField('plugin');
-        $field->setAttributes(XMLDB_TYPE_CHAR, '166', null, XMLDB_NOTNULL, null, null, null, null, 'id');
-
-    /// Launch add field plugin
-        $result = $result && add_field($table, $field);
-
-    /// Define index plugin_idx (not unique) to be dropped form elis_scheduled_tasks
-        $index = new XMLDBIndex('plugin_idx');
-        $index->setAttributes(XMLDB_INDEX_NOTUNIQUE, array('plugintype', 'pluginname', 'taskname'));
-
-    /// Launch drop index plugin_idx
-        $result = $result && drop_index($table, $index);
-
-    /// Define index plugin_idx (not unique) to be added to elis_scheduled_tasks
-        $index = new XMLDBIndex('plugin_idx');
-        $index->setAttributes(XMLDB_INDEX_NOTUNIQUE, array('plugin', 'taskname'));
-
-    /// Launch add index plugin_idx
-        $result = $result && add_index($table, $index);
-
-    /// Define field plugin to be dropped from elis_scheduled_tasks
-        $field = new XMLDBField('plugintype');
-
-    /// Launch drop field plugin
-        $result = $result && drop_field($table, $field);
-
-    /// Define field plugin to be dropped from elis_scheduled_tasks
-        $field = new XMLDBField('pluginname');
-
-    /// Launch drop field plugin
-        $result = $result && drop_field($table, $field);
-
-        /***********************************************************************
-         * Change callfunction from text to char
-         **********************************************************************/
-
-    /// Changing type of field callfunction on table elis_scheduled_tasks to char
-        $field = new XMLDBField('callfunction');
-        $field->setAttributes(XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, null, null, 'callfile');
-
-    /// Launch change of type for field callfunction
-        $result = $result && change_field_type($table, $field);
-    }
-
-    if ($result && $oldversion < 2011030403) {
-
-    /// Define field startdate to be added to elis_scheduled_tasks
-        $table = new XMLDBTable('elis_scheduled_tasks');
-        $field = new XMLDBField('startdate');
-        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, null, 'runsremaining');
-
-    /// Launch add field startdate
-        $result = $result && add_field($table, $field);
-    }
-
     if ($result && $oldversion < 2011063000) {
 
         // Define table elis_scheduled_tasks to be created
