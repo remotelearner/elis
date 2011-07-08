3795b901a91c881aa06b48cb59c990bbddc64a4f
brendan <brendan@brendan-desktop.(none)>
2011-07-08 09:35:48 -0400

    ELIS-2061 Initial commit of new course rollover code

---
+++ elis/core/lib/rollover/lib.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Main library for performing a course rollover
+ *
+ * ELIS(TM): Enterprise Learning Intelligence Suite
+ * Copyright (C) 2008-2011 Remote-Learner.net Inc (http://www.remote-learner.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    elis
+ * @subpackage core
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2008-2011 Remote Learner.net Inc http://www.remote-learner.net
+ *
+ */
+
+require_once($CFG->dirroot.'/backup/util/includes/restore_includes.php');
+require_once(elis::lib('rollover/backup/rollover_backup_controller.class.php'));
+require_once(elis::lib('rollover/restore/rollover_restore_controller.class.php'));
+
+/**
+ * Roll over an existing Moodle course into a new one using a customized backup
+ * and restore routine
+ *
+ * @param int $courseid Id of the course we are copying
+ * @param int $categoryid
+ * @return int|boolean Id of the newly created course
+ */
+function course_rollover($courseid, $categoryid = NULL) {
+    global $DB;
+
+    if ($course = $DB->get_record('course', array('id' => $courseid))) {
+        //perform backup
+        $controller = new rollover_backup_controller($courseid);
+        $controller->execute_plan();
+
+        //get directory name for use in restore
+        $backupid = $controller->get_backupid();
+    
+        //start a database transaction to make sure restore is atomic, etc
+        $transaction = $DB->start_delegated_transaction();
+
+        //create the new course
+        $result = restore_dbops::create_new_course($course->fullname, $course->shortname, $course->category);
+
+        //restore the content into it within the current transaction
+        $controller = new rollover_restore_controller($backupid, $courseid);
+        $controller->execute_precheck();
+        $controller->execute_plan();
+
+        try {
+            //try to finalize
+            $transaction->allow_commit();
+        } catch (dml_transaction_exception $e) {
+            //failure
+            $result = false;
+        }
+
+        return $result;
+    }
+}
