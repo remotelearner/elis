960bbfa9a7c9057ab42aeb036cbc670b3c2ea8e9
Linda Vanderbaan <linda.vanderbaan@remote-learner.net>
2011-07-13 11:23:22 -0400

    ELIS-2066 fixed userset page to be able to add/edit/display custom field

--- elis/program/form/usersetform.class.php
+++ elis/program/form/usersetform.class.php
@@ -85,7 +85,6 @@ class usersetform extends cmform {
                 $lastcat = $rec->categoryid;
                 $mform->addElement('header', "category_{$lastcat}", htmlspecialchars($rec->categoryname));
             }
-            echo '<br>adding field: ';
             manual_field_add_form_element($this, $mform, $context, $field);
         }
 
