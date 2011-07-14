50461ca064dc8a921fd6c9c09d3d750051760cd3
Marko Vidberg <marko.vidberg@remote-learner.net>
2011-07-14 10:06:31 -0400

    ELIS-2064 added install code

--- elis/program/plugins/pre_post_test/lib.php
+++ elis/program/plugins/pre_post_test/lib.php
@@ -24,59 +24,6 @@
  *
  */
 
-function pre_post_test_install() {
-    global $CFG;
+define('PRE_TEST_FIELD', '_elis_course_pretest');
+define('POST_TEST_FIELD', '_elis_course_posttest');
 
-    require_once elispm::lib('setup.php');
-    require_once elis::lib('data/customfield.class.php');
-
-    $course_ctx_lvl = context_level_base::get_custom_context_level('course', 'elis_program');
-
-    // Pre-test field
-    $field = new field();
-    $field->shortname = '_elis_course_pretest';
-    $field->name = get_string('pre_test_field_name', 'crlm_pre_post_test');
-    $field->datatype = 'char';
-
-    $category = new field_category();
-    $category->name = get_string('pre_post_test_category_name', 'crlm_pre_post_test');
-
-    $field = field::ensure_field_exists_for_context_level($field, 'course', $category);
-
-    // make sure 'manual' is an owner
-    if (!isset($field->owners['manual'])) {
-        $owner = new field_owner();
-        $owner->fieldid = $field->id;
-        $owner->plugin = 'manual';
-        $owner->param_view_capability = '';
-        $owner->param_edit_capability = '';
-        $owner->param_control = 'menu';
-        $owner->param_options_source = 'completion_elements';
-        $owner->add();
-    }
-
-    // Post-test field
-    $field = new field();
-    $field->shortname = '_elis_course_posttest';
-    $field->name = get_string('post_test_field_name', 'crlm_pre_post_test');
-    $field->datatype = 'char';
-
-    $category = new field_category();
-    $category->name = get_string('pre_post_test_category_name', 'crlm_pre_post_test');
-
-    $field = field::ensure_field_exists_for_context_level($field, 'course', $category);
-
-    // make sure 'manual' is an owner
-    if (!isset($field->owners['manual'])) {
-        $owner = new field_owner();
-        $owner->fieldid = $field->id;
-        $owner->plugin = 'manual';
-        $owner->param_view_capability = '';
-        $owner->param_edit_capability = '';
-        $owner->param_control = 'menu';
-        $owner->param_options_source = 'completion_elements';
-        $owner->add();
-    }
-
-    return true;
-}
--- elis/program/plugins/pre_post_test/version.php
+++ elis/program/plugins/pre_post_test/version.php
@@ -1,3 +1,3 @@
 <?php
-$plugin->version = 2011070801;
+$plugin->version = 2011071400;
 
