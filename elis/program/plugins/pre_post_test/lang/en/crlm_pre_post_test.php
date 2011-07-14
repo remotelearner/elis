f1b400911a6bb9b168e10351e471de7ae195d25e
Marko Vidberg <marko.vidberg@remote-learner.net>
2011-07-14 13:17:11 -0400

    ELIS-2064 delete incorrectly named languages files

--- elis/program/plugins/pre_post_test/lang/en/crlm_pre_post_test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$string['pre_post_test_category_name'] = 'Pre- and Post-tests';
-$string['post_test_field_name'] = 'Post-test';
-$string['pre_test_field_name'] = 'Pre-test';
--- elis/program/plugins/pre_post_test/lang/es_mx/crlm_pre_post_test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$string['pre_post_test_category_name'] = 'Examenes Previas-y posteriores';
-$string['post_test_field_name'] = 'Examen Previas';
-$string['pre_test_field_name'] = 'Examen posterior';
