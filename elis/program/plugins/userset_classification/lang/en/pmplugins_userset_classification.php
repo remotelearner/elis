76674925cfe311f65c24bf4f52b8566cd7346281
Marko Vidberg <marko.vidberg@remote-learner.net>
2011-07-14 14:32:40 -0400

    ELIS-2008 initial port of clusterclassification plugin and class

---
+++ elis/program/plugins/userset_classification/lang/en/pmplugins_userset_classification.php
@@ -0,0 +1,19 @@
+<?php
+
+$string['pluginname'] = 'Userset Classification';
+$string['add_cluster_classification'] = 'Add userset classification';
+$string['alfresco_shared_folder'] = 'Automatically create an Alfresco shared organization folder';
+$string['autoenrol_curricula'] = 'Autoenrol users in curricula';
+$string['autoenrol_groups'] = 'Autoenrol users in groups';
+$string['autoenrol_groupings'] = 'Autoenrol users in groupings';
+$string['autoenrol_tracks'] = 'Autoenrol users in tracks';
+$string['child_classification'] = 'Default classification for child clusters';
+$string['classification_field_name'] = 'Userset classification';
+$string['classification_category_name'] = 'Userset classification';
+$string['cluster_classification'] = 'Userset classification';
+$string['delete_cluster_classification'] = 'Delete userset classification';
+$string['manageclusterclassifications'] = 'Userset classifications';
+$string['name'] = 'Name';
+$string['same_classification'] = 'Same classification';
+$string['shortname'] = 'Short name';
+
---
+++ elis/program/plugins/userset_classification/lang/en_us/pmplugins_userset_classification.php
@@ -0,0 +1,7 @@
+<?php
+
+$string['autoenrol_curricula'] = 'Autoenroll users in curricula';
+$string['autoenrol_groups'] = 'Autoenroll users in groups';
+$string['autoenrol_groupings'] = 'Autoenroll users in groupings';
+$string['autoenrol_tracks'] = 'Autoenroll users in tracks';
+
