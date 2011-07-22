16d33979007b6b5d5e1f9bcad055b94dae734a33
Hubert Chathi <hubert@remote-learner.net>
2011-07-22 17:16:20 -0400

    ELIS-2832 refactor code to remove code duplication

--- elis/program/lib/lib.php
+++ elis/program/lib/lib.php
@@ -659,73 +659,59 @@ function pm_moodle_user_to_pm($mu) {
         $mu->idnumber = $mu->username;
         $DB->update_record('user', $mu);
     }
+
+    // skip user if no ID number set
     if (empty($mu->idnumber)) {
         return true;
-    } else if ($cu = $DB->get_record(user::TABLE, array('idnumber' => $mu->idnumber))) {
-        $cu = new user($cu);
-
-        // synchronize any profile changes
-        $cu->username = $mu->username;
-        $cu->password = $mu->password;
-        $cu->idnumber = $mu->idnumber;
-        $cu->firstname = $mu->firstname;
-        $cu->lastname = $mu->lastname;
-        $cu->email = $mu->email;
-        $cu->address = $mu->address;
-        $cu->city = $mu->city;
-        $cu->country = $mu->country;
-        $cu->phone = empty($mu->phone1)?empty($cu->phone)? '': $cu->phone: $mu->phone1;
-        $cu->phone2 = empty($mu->phone2)?empty($cu->phone2)? '': $cu->phone2: $mu->phone2;
-        $cu->language = empty($mu->lang)?empty($cu->language)? '': $cu->language: $mu->lang;
-        $cu->timemodified = time();
-
-        //todo: implement this section once necessary profile field code is available
-        // synchronize custom profile fields
-        //profile_load_data($mu);
-        //$fields = field::get_for_context_level(context_level_base::get_custom_context_level('user', 'block_curr_admin'));
-        //$fields = $fields ? $fields : array();
-        //require_once (CURMAN_DIRLOCATION . '/plugins/moodle_profile/custom_fields.php');
-        //foreach ($fields as $field) {
-        //    $field = new field($field);
-        //    if (isset($field->owners['moodle_profile']) && $field->owners['moodle_profile']->exclude == cm_moodle_profile::sync_from_moodle) {
-        //        $fieldname = "field_{$field->shortname}";
-        //        $cu->$fieldname = $mu->{"profile_field_{$field->shortname}"};
-        //    }
-        //}
-        $cu->save();
-     } else {
-        $cu = new user();
-        $cu->username = $mu->username;
-        $cu->password = $mu->password;
-        $cu->idnumber = $mu->idnumber;
-        $cu->firstname = $mu->firstname;
-        $cu->lastname = $mu->lastname;
-        $cu->email = $mu->email;
-        $cu->address = $mu->address;
-        $cu->city = $mu->city;
-        $cu->country = $mu->country;
-        $cu->phone = $mu->phone1;
-        $cu->phone2 = $mu->phone2;
-        $cu->language = $mu->lang;
-        $cu->transfercredits = 0;
-        $cu->timecreated = $cu->timemodified = time();
-
-        //todo: implement this section once necessary profile field code is available
-        // synchronize profile fields
-        //profile_load_data($mu);
-        //$fields = field::get_for_context_level(context_level_base::get_custom_context_level('user', 'block_curr_admin'));
-        //$fields = $fields ? $fields : array();
-        //require_once (CURMAN_DIRLOCATION . '/plugins/moodle_profile/custom_fields.php');
-        //foreach ($fields as $field) {
-        //    $field = new field($field);
-        //    if (isset($field->owners['moodle_profile']) && $field->owners['moodle_profile']->exclude == cm_moodle_profile::sync_from_moodle) {
-        //        $fieldname = "field_{$field->shortname}";
-        //        $cu->$fieldname = $mu->{"profile_field_{$field->shortname}"};
-        //    }
-        //}
-
-        $cu->save();
     }
+
+    // find the PM user with the same idnumber
+    $cu = user::find(new field_filter('idnumber', $mu->idnumber));
+    if ($cu->valid()) {
+        $cu = $cu->current();
+    } else {
+        // if no such PM user exists, create a new one
+        $cu = new user();
+        $cu->transfercredits = 0;
+        $cu->timecreated = time();
+    }
+
+    // synchronize standard fields
+    $cu->username = $mu->username;
+    $cu->password = $mu->password;
+    $cu->idnumber = $mu->idnumber;
+    $cu->firstname = $mu->firstname;
+    $cu->lastname = $mu->lastname;
+    $cu->email = $mu->email;
+    $cu->address = $mu->address;
+    $cu->city = $mu->city;
+    $cu->country = $mu->country;
+    if (!empty($mu->phone1)) {
+        $cu->phone = $mu->phone1;
+    }
+    if (!empty($mu->phone2)) {
+        $cu->phone2 = $mu->phone2;
+    }
+    if (!empty($mu->lang)) {
+        $cu->language = $mu->lang;
+    }
+    $cu->timemodified = time();
+
+    // synchronize custom profile fields
+    profile_load_data($mu);
+    $fields = field::get_for_context_level(context_level_base::get_custom_context_level('user', 'elis_program'));
+    $fields = $fields ? $fields : array();
+    require_once(elis::plugin_file('elisfields_moodle_profile', 'custom_fields.php'));
+    foreach ($fields as $field) {
+        $field = new field($field);
+        if (isset($field->owners['moodle_profile']) && $field->owners['moodle_profile']->exclude == cm_moodle_profile::sync_from_moodle) {
+            $fieldname = "field_{$field->shortname}";
+            $cu->$fieldname = $mu->{"profile_field_{$field->shortname}"};
+        }
+    }
+
+    $cu->save();
+
     return true;
 }
 
