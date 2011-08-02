19c716d0e06dfdce442b3c6f5cf767c39a3879d3
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-08-02 13:40:19 -0400

    ELIS-2970: ELIS User's username must be lowercase to allow login.

--- elis/program/lib/data/user.class.php
+++ elis/program/lib/data/user.class.php
@@ -199,6 +199,7 @@ class user extends data_object_with_custom_fields {
         if(!empty($data->id_same_user)) {
             $data->username = $data->idnumber;
         }
+        $data->username = moodle_strtolower($data->username);
 
         $this->_load_data_from_record($data, true);
     }
