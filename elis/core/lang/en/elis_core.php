8c78f9288292d8ff33640d8e99ec76604765125e
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-08-16 16:04:04 -0400

    ELIS-2571: Port /elis/core/lib/filtering/userprofilematch.php filter to ELIS 2.0 and add default field labels to language strings in elis_core.

--- elis/core/lang/en/elis_core.php
+++ elis/core/lang/en/elis_core.php
@@ -32,6 +32,27 @@ $string['data_object_validation_unique'] = '{$a->tablename} record must have uni
 $string['elis'] = 'ELIS';
 $string['elisversion'] = '<strong>ELIS Version:</strong> {$a}';
 $string['finish'] = 'Finish';
+
+// Default user profile field labels for userprofilematch filter - can override
+$string['fld_auth'] = 'Authentication';
+$string['fld_city'] = 'City/town';
+$string['fld_confirmed'] = 'Confirmed';
+$string['fld_country'] = 'Country';
+$string['fld_coursecat'] = 'Course category';
+$string['fld_courserole'] = 'Course role';
+$string['fld_email'] = 'Email address';
+$string['fld_firstaccess'] = 'First access';
+$string['fld_firstname'] = 'First name';
+$string['fld_fullname'] = 'Full name';
+$string['fld_idnumber'] = 'ID number';
+$string['fld_lang'] = 'Preferred language';
+$string['fld_lastaccess'] = 'Last access';
+$string['fld_lastlogin'] = 'Last login';
+$string['fld_lastname'] = 'Last name';
+$string['fld_systemrole'] = 'System role';
+$string['fld_timemodified'] = 'Last modified';
+$string['fld_username'] = 'Username';
+
 $string['invalidid'] = 'Invalid ID';
 $string['invalidoperator'] = 'Invalid Operator';
 $string['pluginname'] = 'ELIS Core';
