05f72304dd2d7997a321263d8619b634f13467cd
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-08-22 16:21:55 -0400

    ELIS-2585: Port registrants_by_course report to ELIS 2.0.

---
+++ blocks/php_report/instances/registrants_by_course/lang/en/rlreport_registrants_by_course.php
@@ -0,0 +1,32 @@
+<?php
+
+$string['allcurricula'] = 'All curricula';
+$string['anytime'] = 'Any Time';
+
+$string['column_id'] = 'ID Number';
+$string['column_start_date'] = 'Start Date';
+$string['column_student'] = 'Student Name';
+
+$string['displayname'] = 'New Registrants by Course Report';
+
+$string['filter_curriculum'] = 'Select the curricula or choose all curricula';
+$string['filter_date_range'] = 'Select the date range for this report';
+
+$string['grouping_curriculum'] = 'Curriculum';
+$string['grouping_course'] = 'Course Name';
+$string['grouping_class'] = 'Class ID';
+
+$string['header_date_range'] = 'New Student Registrants By Course';
+
+$string['na'] = 'Not available';
+
+$string['registrants_by_course_curriculum'] = 'Select Curriculum';
+$string['registrants_by_course_curriculum_help'] = '<p align="center"><b>Registrants By Course Report Help</b></p>
+
+<p>To select courses in a curriculum to display in the report output, select the name of the curriculum you desire.</p>';
+
+$string['registrants_by_course_date'] = 'Select Date';
+$string['registrants_by_course_date_help'] = '<p align="center"><b>Registrants By Course Report Help</b></p>
+
+<p>If you choose to select a date range here, the report output will only show courses with student class enrollments in the specified range.</p>';
+
