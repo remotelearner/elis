544db86db046378a13fe22619bbbdd12555a49af
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-08-22 16:53:44 -0400

    ELIS-2586: Port registrants_by_student report to ELIS 2.0.

---
+++ blocks/php_report/instances/registrants_by_student/lang/en/rlreport_registrants_by_student.php
@@ -0,0 +1,34 @@
+<?php
+
+$string['anytime'] = 'Any Time';
+
+$string['column_class'] = 'Class ID';
+$string['column_course'] = 'Course Name';
+$string['column_start_date'] = 'Start Date';
+
+$string['displayname'] = 'New Registrants by Student Report';
+
+$string['filter_cluster'] = 'Select the organizational Userset to view';
+$string['filter_date_range'] = 'Select the date range for this report';
+
+$string['grouping_userid'] = 'User IDnumber';
+$string['grouping_username'] = 'Student Name';
+
+$string['header_date_range'] = 'New Student Registrants';
+
+$string['na'] = 'Not available';
+
+$string['registrants_by_student_cluster'] = 'Select Userset';
+$string['registrants_by_student_cluster_help'] = '<p align="center"><b>Registrants By Student Report Help</b></p>
+
+<p>To select a userset of students to display in the report output, select either "is equal to" or "isn\'t equal to" in the first pulldown and then select the name of the userset you desire.</p>
+
+<p>"is equal to" will display all students belonging to the specified userset.</p>
+
+<p>"isn\'t equal to" will display all students NOT belonging to the specified userset.</p>';
+
+$string['registrants_by_student_date'] = 'Select Date';
+$string['registrants_by_student_date_help'] = '<p align="center"><b>Registrants By Student Report Help</b></p>
+
+<p>If you choose to select a date range here, the report output will only show students with class enrollments in the specified range.</p>';
+
