66143643f7127398deb622ef899d360717c9863c
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-08-25 14:15:58 -0400

    ELIS-2578: Port course_completion_by_cluster to ELIS 2.0 + language string 'cluster_reports' -> Userset ...

---
+++ blocks/php_report/instances/course_completion_by_cluster/lang/en/rlreport_course_completion_by_cluster.php
@@ -0,0 +1,78 @@
+<?php
+
+$string['cluster_leaders'] = 'Userset Leader(s):';
+$string['column_class'] = 'Class';
+$string['column_cluster'] = 'Userset';
+$string['column_completed'] = 'Completed';
+$string['column_course'] = 'Course';
+$string['column_curriculum'] = 'Curriculum';
+$string['column_curriculum_completed'] = 'Completed';
+$string['column_grade'] = 'Current Grade';
+$string['column_idnumber'] = 'User ID';
+$string['column_numcomplete'] = 'Completion Elements Completed';
+$string['column_option_completion'] = 'Completion';
+$string['column_option_curriculum'] = 'Curriculum';
+$string['column_option_status'] = 'Status';
+$string['column_required'] = 'Required';
+$string['column_user_name'] = 'User Name';
+$string['columns_options_heading'] = 'Optional information';
+$string['completed'] = 'Completed';
+$string['completed_no'] = '(Not Completed)';
+$string['completed_yes'] = '(Completed - {$a})';
+$string['completionstatus_options_heading'] = 'Completion status';
+
+$string['course_completion_by_cluster'] = 'Select Userset';
+$string['course_completion_by_cluster_help'] = '<p align="center"><b>Course Completion by Userset Tree Help</b></p>
+
+<p>Single Userset selection: Use the drop-down and select a Userset.</p>
+<p>Multiple Userset selection: Click Enable Tree and...</p>
+<p>To add a Userset, check the box beside it.</p>
+<p>If there are any child Usersets, selecting a parent Userset will select all the child Usersets.</p>
+<p>To unselect a child Userset, expand the parent Userset\'s tree and uncheck the child Userset.</p>';
+
+$string['date_format'] = '%d/%m/%Y';
+$string['displayname'] = 'Course Completion By Userset Report';
+
+$string['enable_dropdown'] = 'Enable Dropdown';
+$string['enable_tree'] = 'Enable Tree';
+
+$string['failed'] = 'Failed';
+$string['filter_cluster'] = 'Select the organization/Userset or all organizations/Usersets';
+$string['filter_clusterrole'] = 'Userset Leader Role';
+$string['filter_completed'] = 'Show completed courses';
+$string['filter_courses'] = 'Choose whether to show only completed courses, only incomplete courses, or both';
+$string['filter_incomplete'] = 'Show incomplete courses';
+$string['formatted_grade'] = '{$a}%';
+$string['formatted_grade_csv'] = '{$a}';
+
+$string['grouping_cluster'] = 'Userset: ';
+$string['grouping_completed'] = 'Completed';
+$string['grouping_curriculum'] = 'Curriculum: ';
+$string['grouping_not_completed'] = 'Not Completed';
+$string['grouping_userid'] = 'User ID';
+$string['grouping_username'] = 'User Name';
+
+$string['header_cluster'] = 'Organization/Userset';
+
+$string['na'] = 'N/A';
+$string['no'] = 'No';
+$string['non_curriculum_courses'] = 'Non-Curriculum Courses';
+$string['numcomplete_tally'] = '{$a->numcomplete} / {$a->numtotal}';
+
+$string['passed'] = 'Passed';
+$string['percent_symbol'] = '%';
+
+$string['required_no'] = 'No';
+$string['required_yes'] = 'Yes';
+
+$string['show_completed_courses'] = 'Show completed courses';
+$string['show_incomplete_courses'] = 'Show incomplete courses';
+$string['stustatus_notcomplete'] = 'Not Completed';
+$string['stustatus_notenrolled'] = 'Not Enrolled';
+$string['stustatus_passed'] = 'Passed - {$a}';
+$string['stustatus_failed'] = 'Failed - {$a}';
+
+$string['tree_help'] = 'Select Usersets';
+
+$string['yes'] = 'Yes';
+
