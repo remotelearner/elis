65905a92ab3b6ece9ef1a2558bf9cc8b5ba50f8a
Marko Vidberg <marko.vidberg@remote-learner.net>
2011-08-30 13:33:10 -0400

    ELIS-3101 initial port of curriculum archive plugin

---
+++ elis/program/plugins/archive/lang/en/pmplugins_archive.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Archive Program';
+$string['archive_category_name'] = 'Archive Settings';
+$string['archive_field_name'] = 'Archive Program';
+$string['archive_field_name_help'] = 'Enabling this will cause the dashboard to only show this program and associated course descriptions when the "Archived Learning Plans" tab is selected.';
+
