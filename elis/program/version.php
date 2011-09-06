70628c4eb286f0417319f234b894f21b81e4b760
Hubert Chathi <hubert@remote-learner.net>
2011-09-06 13:42:41 -0400

    ELIS-3157 fix capability name

--- elis/program/version.php
+++ elis/program/version.php
@@ -2,5 +2,5 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2011081100;
-$plugin->release = '2.0.0-PRERELEASE (Build: 20110325)';
+$plugin->version = 2011090600;
+$plugin->release = '2.0.0-PRERELEASE (Build: 20110906)';
