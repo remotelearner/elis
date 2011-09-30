364a9339e2fea2134b1b6032e218ae07cf95ba97
Tyler Bannister <tyler.bannister@remote-learner.net>
2011-09-30 08:30:51 -0400

    PREPCL-479 - Removing test harness file for security reasons.

--- elis/program/plugins/results_engine/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Delete this file when test harness is no longer needed.
-require_once(dirname(__FILE__) .'/../../lib/setup.php');
-require_once(dirname(__FILE__) .'/lib.php');
-
-results_engine_cron();
