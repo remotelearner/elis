3615a1a8b0c64f748227914189ee918a1592eda8
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-10-25 16:47:07 -0400

    ELIS-3400,ELIS-3460: Add cluster(UserSet) users to auto-enrollable track classes when associated via Moodle user profile field(s), also add check for pre-requisites.

--- elis/program/lib/data/clusterassignment.class.php
+++ elis/program/lib/data/clusterassignment.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2010 Remote-Learner.net Inc (http://www.remote-learner.net)
+ * Copyright (C) 2008-2011 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,10 +17,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    elis
- * @subpackage curriculummanagement
+ * @subpackage programmanagement
  * @author     Remote-Learner.net Inc
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2008-2010 Remote Learner.net Inc http://www.remote-learner.net
+ * @copyright  (C) 2008-2011 Remote Learner.net Inc http://www.remote-learner.net
  *
  */
 defined('MOODLE_INTERNAL') || die();
@@ -120,153 +120,165 @@ class clusterassignment extends elis_data_object {
      * @param  int  $clusterid  A specific cluster / user set id to filter
      *                          on for consideration, or all users if zero
      */
-	static function update_enrolments($userid = 0, $clusterid = 0) {
-	    global $DB;
+    static function update_enrolments($userid = 0, $clusterid = 0) {
+        global $DB;
+        //error_log("/elis/program/lib/data/clusterassignment.class.php::update_enrolments({$userid}, {$clusterid})");
 
-	    //convert provided parameters to SQL conditions
-	    $extraconditions = array();
-	    $extraparams = array();
+        //convert provided parameters to SQL conditions
+        $extraconditions = array();
+        $extraparams = array();
 
-	    if (!empty($userid)) {
-	    	$extraconditions[] = 'u.id = ?';
-	    	$extraparams[] = $userid;
-	    }
+        if (!empty($userid)) {
+            $users = array($userid);
+            $extraconditions[] = 'u.id = ?';
+            $extraparams[] = $userid;
+        } else {
+            $users = clusterassignment::find(new field_filter('clusterid', $clusterid));
+        }
 
-	    if (!empty($clusterid)) {
-	        $extraconditions[] = 'clu.clusterid = ?';
-	        $extraparams[] = $clusterid;
-	    }
+        if (!empty($clusterid)) {
+            $extraconditions[] = 'clu.clusterid = ?';
+            $extraparams[] = $clusterid;
+        }
 
-	    //conbine conditions into a where condition
-	    $extrawhere = '';
-	    if (!empty($extraconditions)) {
-	    	$extrawhere = ' AND '.implode(' AND ', $extraconditions);
-	    }
+        $extrawhere = '';
+        if (!empty($extraconditions)) {
+            $extrawhere = ' AND '. implode(' AND ', $extraconditions);
+        }
 
-	    //use the current time as the time created and modified for curriculum
-	    //assignments
-	    $timenow = time();
+        //use the current time as the time created and modified for curriculum
+        //assignments
+        $timenow = time();
 
-	    //assign to curricula based on user-cluster and cluster-curriculum
-	    //associations
-	    $sql = "INSERT INTO {".curriculumstudent::TABLE."}
-	           (userid, curriculumid, timecreated, timemodified)
-	           SELECT DISTINCT u.id, clucur.curriculumid, {$timenow}, {$timenow}
-	           FROM {".clusterassignment::TABLE."} clu
-	           JOIN {".user::TABLE."} u ON u.id = clu.userid
-	           JOIN {".clustercurriculum::TABLE."} clucur
-	             ON clucur.clusterid = clu.clusterid
-	           LEFT JOIN {".curriculumstudent::TABLE."} ca
-	             ON ca.userid = u.id
-	             AND ca.curriculumid = clucur.curriculumid
-	           WHERE ca.curriculumid IS NULL
-	             AND clucur.autoenrol = 1
-	             {$extrawhere}";
-	    $DB->execute($sql, $extraparams);
+        //assign to curricula based on user-cluster and cluster-curriculum
+        //associations
+        $sql = "INSERT INTO {".curriculumstudent::TABLE."}
+                    (userid, curriculumid, timecreated, timemodified)
+                SELECT DISTINCT u.id, clucur.curriculumid, {$timenow}, {$timenow}
+                  FROM {".clusterassignment::TABLE."} clu
+                  JOIN {".user::TABLE."} u ON u.id = clu.userid
+                  JOIN {".clustercurriculum::TABLE."} clucur
+                    ON clucur.clusterid = clu.clusterid
+             LEFT JOIN {".curriculumstudent::TABLE."} ca
+                    ON ca.userid = u.id
+                   AND ca.curriculumid = clucur.curriculumid
+                 WHERE ca.curriculumid IS NULL
+                   AND clucur.autoenrol = 1
+                   {$extrawhere}";
+        $DB->execute($sql, $extraparams);
 
-	    //assign to curricula based on user-cluster and cluster-track
-	    //associations (assigning a user to a track auto-assigns them to
-	    //the track's curriculum, track assignment happens below)
-	    $sql = "INSERT INTO {".curriculumstudent::TABLE."}
-	            (userid, curriculumid, timecreated, timemodified)
-	            SELECT DISTINCT u.id, trk.curid, {$timenow}, {$timenow}
-	            FROM {".clusterassignment::TABLE."} clu
-	            JOIN {".user::TABLE."} u
-	              ON u.id = clu.userid
-	            JOIN {".clustertrack::TABLE."} clutrk
-	              ON clutrk.clusterid = clu.clusterid
-	            JOIN {".track::TABLE."} trk ON
-	              clutrk.trackid = trk.id 
-	            LEFT JOIN {".curriculumstudent::TABLE."} ca
-	              ON ca.userid = u.id
-	              AND ca.curriculumid = trk.curid
-	            WHERE ca.curriculumid IS NULL
-	              AND clutrk.autoenrol = 1
-	              {$extrawhere}";
-	    $DB->execute($sql, $extraparams); 
+        //assign to curricula based on user-cluster and cluster-track
+        //associations (assigning a user to a track auto-assigns them to
+        //the track's curriculum, track assignment happens below)
+        $sql = "INSERT INTO {".curriculumstudent::TABLE."}
+                    (userid, curriculumid, timecreated, timemodified)
+                SELECT DISTINCT u.id, trk.curid, {$timenow}, {$timenow}
+                  FROM {".clusterassignment::TABLE."} clu
+                  JOIN {".user::TABLE."} u
+                    ON u.id = clu.userid
+                  JOIN {".clustertrack::TABLE."} clutrk
+                    ON clutrk.clusterid = clu.clusterid
+                  JOIN {".track::TABLE."} trk
+                    ON clutrk.trackid = trk.id
+             LEFT JOIN {".curriculumstudent::TABLE."} ca
+                    ON ca.userid = u.id
+                   AND ca.curriculumid = trk.curid
+                 WHERE ca.curriculumid IS NULL
+                   AND clutrk.autoenrol = 1
+                   {$extrawhere}";
+        $DB->execute($sql, $extraparams);
 
-	    //this represents the tracks that users will be assigned to
-	    //based on user-cluster and cluster-track associations
-	    //(actual assignment happens below)
-	    $exists = "EXISTS (SELECT DISTINCT u.id, clutrk.trackid
-	               FROM {".clusterassignment::TABLE."} clu
-	               JOIN {".user::TABLE."} u
-	                 ON u.id = clu.userid
-	               JOIN {".clustertrack::TABLE."} clutrk
-	                 ON clutrk.clusterid = clu.clusterid
-	               LEFT JOIN {".usertrack::TABLE."} ta
-	                 ON ta.userid = u.id
-	                 AND ta.trackid = clutrk.trackid
-	               WHERE ta.trackid IS NULL
-	                 AND clutrk.autoenrol = 1
-	                 AND outerta.trackid = clutrk.trackid
-	                 {$extrawhere})";
+        //this represents the tracks that users will be assigned to
+        //based on user-cluster and cluster-track associations
+        //(actual assignment happens below)
+        $exists = "EXISTS (SELECT DISTINCT u.id, clutrk.trackid
+                             FROM {".clusterassignment::TABLE."} clu
+                             JOIN {".user::TABLE."} u
+                               ON u.id = clu.userid
+                             JOIN {".clustertrack::TABLE."} clutrk
+                               ON clutrk.clusterid = clu.clusterid
+                        LEFT JOIN {".usertrack::TABLE."} ta
+                               ON ta.userid = u.id
+                              AND ta.trackid = clutrk.trackid
+                            WHERE ta.trackid IS NULL
+                              AND clutrk.autoenrol = 1
+                              AND outerta.trackid = clutrk.trackid
+	                      {$extrawhere})";
 
-	    /**
-	     * Get autoenrollable classes in the track.  Classes are autoenrollable
-	     * if:
-	     * - the autoenrol flag is set
-	     * - it is the only class in that course slot for the track
-	     */
-	    // group the classes from the same course together
-	    // only select the ones that are the only class for that course in
-	    // the given track, and if the autoenrol flag is set
-	    $sql = "SELECT outerta.classid, outerta.courseid
-	            FROM {".trackassignment::TABLE."} outerta
-	            WHERE {$exists}
-	            GROUP BY courseid
-	            HAVING COUNT(*) = 1
-	              AND MAX(autoenrol) = 1";
+        /**
+         * Get autoenrollable classes in the track.  Classes are autoenrollable
+         * if:
+         * - the autoenrol flag is set
+         * - it is the only class in that course slot for the track
+         */
+        // group the classes from the same course together
+        // only select the ones that are the only class for that course in
+        // the given track, and if the autoenrol flag is set
+        $sql = "SELECT outerta.classid, outerta.courseid, trk.curid
+                  FROM {". trackassignment::TABLE ."} outerta
+                  JOIN {". track::TABLE ."} trk ON trk.id = outerta.trackid
+                 WHERE {$exists}
+              GROUP BY courseid
+                HAVING COUNT(*) = 1 AND MAX(autoenrol) = 1";
 
-	    //go through and assign user(s) to the autoenollable classes
-	    $classes = $DB->get_records_sql($sql, $extraparams);
-	    if (!empty($classes)) {
-	        foreach ($classes as $class) {
-	            $now = time();
-	            // enrol user in each autoenrolable class
-	            $stu_record = new object();
-	            $stu_record->userid = $userid;
-	            $stu_record->classid = $class->classid;
-	            $stu_record->enrolmenttime = $now;
-	            $enrolment = new student($stu_record);
+         //go through and assign user(s) to the autoenollable classes
+         $classes = $DB->get_records_sql($sql, $extraparams);
+         if (!empty($classes)) {
+            foreach ($users as $user) {
+                $userid = is_object($user) ? $user->userid: $user;
+                foreach ($classes as $class) {
+                    // check pre-requisites
+                    $curcrs = new curriculumcourse(
+                                      array('courseid' => $class->courseid,
+                                            'curriculumid' => $class->curid));
+                    if (!$curcrs->prerequisites_satisfied($userid)) {
+                        continue;
+                    }
+                    $now = time();
+                    // enrol user in each autoenrolable class
+                    $stu_record = new object();
+                    $stu_record->userid = $userid;
+                    $stu_record->classid = $class->classid;
+                    $stu_record->enrolmenttime = $now;
+                    $enrolment = new student($stu_record);
 
-	            // catch enrolment limits
-	            try {
-	                $status = $enrolment->save();
-	            } catch (pmclass_enrolment_limit_validation_exception $e) {
-	                // autoenrol into waitlist
-	                $wait_record = new object();
-	                $wait_record->userid = $userid;
-	                $wait_record->classid = $class->classid;
-	                $wait_record->enrolmenttime = $now;
-	                $wait_record->timecreated = $now;
-	                $wait_record->position = 0;
-	                $wait_list = new waitlist($wait_record);
-	                $wait_list->save();
-	                $status = true;
-	            } catch (Exception $e) {
-	                echo cm_error(get_string('record_not_created_reason',
-	                                         'elis_program', $e));
-	            }
-	        }
-	    }
+                    // catch enrolment limits
+                    try {
+                        $enrolment->save();
+                    } catch (pmclass_enrolment_limit_validation_exception $e) {
+                        // autoenrol into waitlist
+                        $wait_record = new object();
+                        $wait_record->userid = $userid;
+                        $wait_record->classid = $class->classid;
+                        $wait_record->enrolmenttime = $now;
+                        $wait_record->timecreated = $now;
+                        $wait_record->position = 0;
+                        $wait_list = new waitlist($wait_record);
+                        $wait_list->save();
+                    } catch (Exception $e) {
+                        echo cm_error(get_string('record_not_created_reason',
+                                                 'elis_program', $e));
+                    }
+                }
+            }
+        }
 
-	    //assign to tracks based on user-cluster and cluster-track
-	    //associations
-	    $sql = "INSERT INTO {".usertrack::TABLE."}
-	            (userid, trackid)
-	            SELECT DISTINCT u.id, clutrk.trackid
-	            FROM {".clusterassignment::TABLE."} clu
-	            JOIN {".user::TABLE."} u
-	              ON u.id = clu.userid
-	            JOIN {".clustertrack::TABLE."} clutrk
-	              ON clutrk.clusterid = clu.clusterid
-	            LEFT JOIN {".usertrack::TABLE."} ta
-	              ON ta.userid = u.id
-	              AND ta.trackid = clutrk.trackid
-	            WHERE ta.trackid IS NULL
-	              AND clutrk.autoenrol = 1
-	              {$extrawhere}";
-	    $DB->execute($sql, $extraparams);
-	}
+        //assign to tracks based on user-cluster and cluster-track
+        //associations
+        $sql = "INSERT INTO {".usertrack::TABLE."}
+                       (userid, trackid)
+                SELECT DISTINCT u.id, clutrk.trackid
+                  FROM {".clusterassignment::TABLE."} clu
+                  JOIN {".user::TABLE."} u
+                    ON u.id = clu.userid
+                  JOIN {".clustertrack::TABLE."} clutrk
+                    ON clutrk.clusterid = clu.clusterid
+             LEFT JOIN {".usertrack::TABLE."} ta
+                    ON ta.userid = u.id
+                   AND ta.trackid = clutrk.trackid
+                 WHERE ta.trackid IS NULL
+                   AND clutrk.autoenrol = 1
+                   {$extrawhere}";
+        $DB->execute($sql, $extraparams);
+    }
 }
