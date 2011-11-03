fc63bfb6da5ec281d6192e54da8dc955947bf24e
Justin Filip <jfilip@remote-learner.ca>
2011-11-03 10:26:39 -0400

    ELIS-3583 ELIS-3582 ELIS-3581 ELIS-3580 ELIS-3579 Created the pre-upgrade script to handle migrating data during an ELIS 1.9 to ELIS 2 upgrade.

--- elis/core/scripts/preupgrade.php
+++ elis/core/scripts/preupgrade.php
@@ -29,53 +29,168 @@
 
 define('CLI_SCRIPT', true);
 
-require_once('../../../config.php');
-
-
+require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
 
 
 /*
-
-The following are queries that can be used to detect the presence of duplicates and also queries that can be safely used to clean up duplicate values.
-
-!!! NOTE: These queries should be wrapped inside a transaction (especially the removal of duplicates).
-
--- Detect duplicates
-
-SELECT contextid, lowerboundary, letter, COUNT(*) count
-FROM mdl_grade_letters
-GROUP BY contextid, lowerboundary, letter
-ORDER BY count DESC
-
-
--- Remove duplicates
-
-CREATE TABLE mdl_grade_letters_temp LIKE mdl_grade_letters;
-
-INSERT INTO mdl_grade_letters_temp (contextid, lowerboundary, letter) SELECT contextid, lowerboundary, letter FROM mdl_grade_letters GROUP BY contextid, lowerboundary, letter;
-
-DROP TABLE mdl_grade_letters;
-
-RENAME TABLE mdl_grade_letters_temp TO mdl_grade_letters;
-
-
--- ======================================================================
-
--- Detect duplicates
-
-SELECT userid, name, value, COUNT(*) count
-FROM mdl_user_preferences
-GROUP BY userid, name, value
-ORDER BY count DESC
-
-
--- Remove duplicates
-
-CREATE TABLE mdl_user_preferences_temp LIKE mdl_user_preferences;
-
-INSERT INTO mdl_user_preferences_temp (userid, name, value) SELECT userid, name, value FROM mdl_user_preferences GROUP BY userid, name, value;
-
-DROP TABLE mdl_user_preferences;
-
-RENAME TABLE mdl_user_preferences_temp TO mdl_user_preferences;
+ * Handle duplicate records in the mdl_grade_letters table.
  */
+
+// Detect if we have any duplicate records before we try to remove duplicates
+$sql = "SELECT contextid, lowerboundary, letter, COUNT(*) count
+        FROM {grade_letters}
+        GROUP BY contextid, lowerboundary, letter
+        ORDER BY count DESC";
+
+if ($rec = $DB->get_records_sql($sql, array(), 0, 1)) {
+    $count = current($rec);
+
+    if ($count->count <= 1) {
+        break;
+    }
+
+    $status = true;
+
+    try {
+        $DB->execute('CREATE TABLE {grade_letters_temp} LIKE {grade_letters}');
+    } catch (Excpetion $e) {
+        $status = false;
+    }
+
+    if ($status) {
+        $tx = $DB->start_delegated_transaction();
+
+        $sql = 'INSERT INTO {grade_letters_temp} (contextid, lowerboundary, letter)
+                SELECT contextid, lowerboundary, letter
+                FROM {grade_letters}
+                GROUP BY contextid, lowerboundary, letter';
+
+        try {
+            $DB->execute($sql);
+        } catch (Exception $e) {
+            $status = false;
+            $tx->rollback($e);
+            $tx->dispose();
+            break;
+        }
+
+        $tx->allow_commit();
+        $tx->dispose();
+    }
+
+    if ($status) {
+        try {
+            $DB->execute('DROP TABLE {grade_letters}');
+            $DB->execute('RENAME TABLE {grade_letters_temp} TO {grade_letters}');
+        } catch (Exception $e) {
+            $status = false;
+        }
+    }
+}
+
+
+/*
+ * Migrate the old Alfresco capability / role assignments to new ELIS Files capabilities.
+ */
+
+if ($status) {
+    $tx = $DB->start_delegated_transaction();
+
+    try {
+        // Find all of the capabilities that are
+        $select = 'capability LIKE :cap AND permission LIKE :perm';
+        $params = array('cap' => 'block/repository:%', 'perm' => CAP_ALLOW);
+
+        if ($rcaps = $DB->get_recordset_select('role_capabilities', $select, $params, 'timemodified ASC', 'id, capability')) {
+            foreach ($rcaps as $rcap) {
+                $rcap->capability = str_replace('block/repository:', 'repository/elis_files:', $rcap->capability);
+                $DB->update_record_raw('role_capabilities', $rcap, true);
+            }
+
+            $rcaps->close();
+        }
+    } catch (Exception $e) {
+        $status = false;
+        $tx->rollback($e);
+    }
+
+    if ($status) {
+        $tx->allow_commit();
+    }
+
+    $tx->dispose();
+}
+
+
+/*
+ * Migrate the old Alfresco repository plugin configuration settings to the new ELIS Files repository plugin.
+ */
+
+if ($status) {
+    $tx = $DB->start_delegated_transaction();
+
+    try {
+    // Find all of the old Alfresco repository plugin capabilities that are set to enabled
+        $select = 'name LIKE :name';
+        $params = array('name' => 'repository_alfresco%');
+
+        if ($cfgs = $DB->get_recordset_select('config', $select, $params, 'name ASC')) {
+            foreach ($cfgs as $cfg) {
+                // We need to create a new entry in the mdl_plugin_config table and remove the mdl_config values
+                $pcfg = new stdClass;
+                $pcfg->plugin = 'elis_files';
+
+                // Soem variables should not be migrated and need to just be deleted
+                if ($cfg->name == 'repository_alfresco_version' || $cfg->name == 'repository_alfresco_cachetime') {
+                    continue;
+                }
+
+                $pcfg->name  = str_replace('repository_alfresco_', '', $cfg->name);
+                $pcfg->value = $cfg->value;
+
+                $DB->insert_record_raw('config_plugins', $pcfg, false, true);
+            }
+
+            $cfgs->close();
+
+            // Delete the old plugin configuration values
+            $DB->delete_records_select('config', $select, $params);
+        }
+    } catch (Exception $e) {
+        $status = false;
+        $tx->rollback($e);
+    }
+
+    if ($status) {
+        $tx->allow_commit();
+    }
+
+    $tx->dispose();
+}
+
+/*
+ * Ensure that if the Alfresco SSO auth plugin is enabled that it is replaced witht the ELIS Files SSO plugin instead.
+ */
+
+if ($status) {
+    $tx = $DB->start_delegated_transaction();
+
+    try {
+        $auth = $DB->get_field('config', 'value', array('name' => 'auth'));
+
+        $auth = str_replace('alfrescosso', 'elisfilessso', $auth, $count);
+
+        if ($count > 0) {
+            $DB->set_field('config', 'value', $auth, array('name' => 'auth'));
+        }
+    } catch (Excpetion $e) {
+        $status = false;
+        $tx->rollback($e);
+    }
+
+    if ($status) {
+        $tx->allow_commit();
+    }
+
+    $tx->dispose();
+}
