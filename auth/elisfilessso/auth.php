c518b3a0a5832af9dd3bd19d34134b57e9a21e24
Justin Filip <jfilip@remote-learner.ca>
2011-11-03 14:46:15 -0400

    ELIS-3607 Cleaned up the formatting of the code some.

--- auth/elisfilessso/auth.php
+++ auth/elisfilessso/auth.php
@@ -1,25 +1,31 @@
 <?php
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Alfresco SSO authentication plug-in.
+ * ELIS Files SSO authentication plugin
  *
- * @package    elis
- * @subpackage File system
- * @copyright  2010 Remote Learner - http://www.remote-learner.net/
- * @author     Justin Filip <jfilip@remote-learner.net>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * ELIS(TM): Enterprise Learning Intelligence Suite
+ * Copyright (C) 2008-2011 Remote-Learner.net Inc (http://www.remote-learner.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    auth
+ * @subpackage elisfilessso
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2008-2011 Remote Learner.net Inc http://www.remote-learner.net
+ *
  */
+
 
 
 if (!defined('MOODLE_INTERNAL')) {
@@ -27,11 +33,11 @@ if (!defined('MOODLE_INTERNAL')) {
 }
 
 
-require_once($CFG->libdir . '/authlib.php');
+require_once($CFG->libdir.'/authlib.php');
 
 
 /**
- * Alfresco SSO authentication plugin.
+ * ELIS Files SSO authentication plugin.
  */
 class auth_plugin_elisfilessso extends auth_plugin_base {
 
@@ -43,7 +49,7 @@ class auth_plugin_elisfilessso extends auth_plugin_base {
 
         $this->authtype = 'elisfilessso';
         $this->config   = get_config('auth/elisfilessso');
-        $this->isactive = file_exists($CFG->dirroot . '/repository/elis_files/ELIS_files_factory.class.php');
+        $this->isactive = file_exists($CFG->dirroot.'/repository/elis_files/ELIS_files_factory.class.php');
     }
 
 
@@ -139,11 +145,8 @@ class auth_plugin_elisfilessso extends auth_plugin_base {
         if (!$this->isactive) {
             echo $OUTPUT->heading(get_string('elisfilesnotfound', 'auth_elisfilessso'));
         } else {
-            // Change this...
-            // get session key??? sesskey=NQfFeeW6qi
-            $url = $CFG->wwwroot . '/admin/repository.php';
-            //echo '<br>url: '.$url;
-            //admin_externalpage_setup('repository', '', null, '', array('action'=>'edit','repos'=>'elis_files'));
+            $url = $CFG->wwwroot.'/admin/repository.php?action=edit&amp;repos=elis_files';
+
             // TODO:  Have it link directly to the edit page if enabled, otherwise this will do...
             echo $OUTPUT->heading(get_string('configureelisfileshere', 'auth_elisfilessso', $url));
         }
@@ -173,37 +176,20 @@ class auth_plugin_elisfilessso extends auth_plugin_base {
      * This method is called from authenticate_user_login() for all enabled auth plugins.
      *
      * @uses $CFG
-     * @param object $user user object, later used for $USER
+     * @param object $user     user object, later used for $USER
      * @param string $username (with system magic quotes)
      * @param string $password plain text password (with system magic quotes)
      */
     function user_authenticated_hook(&$user, $username, $password) {
         global $CFG;
 
-        //TODO this won't work yet, don't forget! may need to include elis_files/lib.php... not sure...
-        /*if (!$this->isactive || !file_exists($CFG->dirroot . '/repository/elis_files/repository.class.php')) {
-            return;
-        }
-*/
-        if (!$this->isactive || !file_exists($CFG->dirroot . '/repository/elis_files/ELIS_files_factory.class.php')) {
+        if (!$this->isactive ) {
             return;
         }
 
-        require_once($CFG->dirroot . '/repository/elis_files/ELIS_files_factory.class.php');
+        require_once($CFG->dirroot.'/repository/elis_files/ELIS_files_factory.class.php');
 
-        if (!$repo = repository_factory::factory()) {
-            return;
-        }
-
-        /*
-        require_once($CFG->dirroot . '/repository/elis_files/ELIS_files.php');
-
-        if (!$repo = new ELIS_files()) {
-            return;
-        }*/
-
-
-        if (!$repo->verify_setup() || !$repo->is_configured()) {
+        if (!$repo = repository_factory::factory('elis_files')) {
             return;
         }
 
@@ -213,5 +199,3 @@ class auth_plugin_elisfilessso extends auth_plugin_base {
         }
     }
 }
-
-?>
