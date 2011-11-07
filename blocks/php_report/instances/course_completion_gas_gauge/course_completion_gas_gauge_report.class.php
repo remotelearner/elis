2a3b52199c239ae8dd145223b5051d8299f5b42e
Brent Boghosian <brent.boghosian@remote-learner.net>
2011-11-07 13:13:56 -0500

    ELIS-3621: Fix 'gas_guage' sub-queries by adding parentheses around filter WHERE conditions.

--- blocks/php_report/instances/course_completion_gas_gauge/course_completion_gas_gauge_report.class.php
+++ blocks/php_report/instances/course_completion_gas_gauge/course_completion_gas_gauge_report.class.php
@@ -110,7 +110,7 @@ class course_completion_gas_gauge_report extends gas_gauge_table_report {
         $filter_sql = $filter_obj->get_sql(false, null, SQL_PARAMS_NAMED);
         $params = array();
         if (isset($filter_sql['where'])) {
-            $sql .= 'WHERE  '. $filter_sql['where'];
+            $sql .= 'WHERE ('. $filter_sql['where'] .')';
             $params = $filter_sql['where_parameters'];
         }
 
@@ -140,7 +140,7 @@ class course_completion_gas_gauge_report extends gas_gauge_table_report {
         $filter_sql = $filter_obj->get_sql(false, null, SQL_PARAMS_NAMED);
         $params = array();
         if (isset($filter_sql['where'])) {
-            $sql .= 'WHERE  '. $filter_sql['where'];
+            $sql .= 'WHERE ('. $filter_sql['where'] .')';
             $params = $filter_sql['where_parameters'];
         }
 
@@ -173,7 +173,7 @@ class course_completion_gas_gauge_report extends gas_gauge_table_report {
         $filter_sql = $filter_obj->get_sql(false, null, SQL_PARAMS_NAMED);
         $params = array();
         if (isset($filter_sql['where'])) {
-            $sql .= 'WHERE  '. $filter_sql['where'];
+            $sql .= 'WHERE ('. $filter_sql['where'] .')';
             $params = $filter_sql['where_parameters'];
         }
 
