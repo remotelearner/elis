402c31418aec7d0b05dee5292fcce93d85acc074
Tim Gusak <tim.gusak@remote-learner.net>
2012-02-24 10:41:56 -0500

    ELIS-4524 User Class Completion language string porting

---
+++ blocks/php_report/instances/user_class_completion/lang/en/rlreport_user_class_completion.php
@@ -0,0 +1,104 @@
+<?php
+
+$string['user_class_completion_username'] = 'Username';
+$string['user_class_completion_username_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the username of users to be included in this report.</p>';
+
+$string['user_class_completion_summarydatafields'] = 'Summary';
+$string['user_class_completion_summarydatafields_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the summary data fields of users to be included in this report.</p>';
+
+$string['user_class_completion_startdate'] = 'Start Date';
+$string['user_class_completion_startdate_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the start date of the user\'s class. Only classes that match this setting will be displayed in the report.</p>';
+
+$string['user_class_completion_reporttitle'] = 'Report Title';
+$string['user_class_completion_reporttitle_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>The text to be displayed in the title bar that spans the top of the report.</p>
+<p>There are some special codes which can be used to dynamically put content in the report:</p>
+<ol>
+<li>%%site%% - The site name</li>
+<li>%%startdate%% - The start day for the report</li>
+<li>%%enddate%% - The end day for the report</li>
+</ol>';
+
+$string['user_class_completion_lastname'] = 'Last Name';
+$string['user_class_completion_lastname_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the lastname of users to be included in this report.</p>';
+
+$string['user_class_completion_language'] = 'Language';
+$string['user_class_completion_language_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the primary language of users to be included in this report.</p>';
+
+$string['user_class_completion_inactive'] = 'Inactive';
+$string['user_class_completion_inactive_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>Select "Yes" if you want the report output to include inactive users.</p>';
+
+$string['user_class_completion_idnumber'] = 'ID number';
+$string['user_class_completion_idnumber_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the ID number of users to be included in this report.</p>';
+
+$string['user_class_completion_fullname'] = 'Full Name';
+$string['user_class_completion_fullname_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the full name of users to be included in this report, in "Firstname Lastname" format.</p>';
+
+$string['user_class_completion_firstname'] = 'First Name';
+$string['user_class_completion_firstname_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the firstname of users to be included in this report.</p>';
+
+$string['user_class_completion_environment'] = 'Environment';
+$string['user_class_completion_environment_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the environment of users to be included in this report.</p>';
+
+$string['user_class_completion_email'] = 'Email';
+$string['user_class_completion_email_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the email address of users to be included in this report.</p>';
+
+$string['user_class_completion_detailheaderfields'] = 'Detail Header Fields';
+$string['user_class_completion_detailheaderfields_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the detail header fields of users to be included in this report.</p>';
+
+$string['user_class_completion_detaildatafields'] = 'Detail Data Fields';
+$string['user_class_completion_detaildatafields_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the detail data fields of users to be included in this report.</p>';
+
+$string['user_class_completion_curriculum'] = 'Program';
+$string['user_class_completion_curriculum_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the program of users to be included in this report.</p>';
+
+$string['user_class_completion_course'] = 'Course Description';
+$string['user_class_completion_course_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the course description of users to be included in this report.</p>';
+
+$string['user_class_completion_country'] = 'Country';
+$string['user_class_completion_country_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the country of users to be included in this report.</p>';
+
+$string['user_class_completion_completionstatus'] = 'Completion Status';
+$string['user_class_completion_completionstatus_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field only allows classes which match the selected value in the report.</p>
+<p>There are four values for this field:</p>
+<ol>
+<li>Passed: The user has passed the class.</li>
+<li>Failed: The user has failed the class.</li>
+<li>In Progress: The user has logged into the class or completed one or more course elements*.</li>
+<li>Not Started: The user has never logged into the class and has not completed any course elements*.</li>
+</ol>
+<p>* Course elements may be marked as completed by course instructors.</p>';
+
+$string['user_class_completion_cluster'] = 'User Set';
+$string['user_class_completion_cluster_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>Single user set selection: Use the drop-down and select a user set.</p>
+<p>Multiple user set selection: Click Enable Tree and...</p>
+<p>To add a user set, check the box beside it.</p>
+<p>If there are any child user sets, selecting a parent user set will select all the child user sets.</p>
+<p>To unselect a child user set, expand the parent user set\'s tree and uncheck the child user set.</p>';
+
+$string['user_class_completion_class'] = 'Class Instance';
+$string['user_class_completion_class_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the class instance of users to be included in this report.</p>';
+
+$string['user_class_completion_city'] = 'City';
+$string['user_class_completion_city_help'] = '<p align="center"><b>User Class Completion Report Help</b></p>
+<p>This field represents the city of users to be included in this report.</p>';
+
