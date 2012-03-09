737a74b3d3e5faaf73818db8a92131b78e4097d8
brendan <brendan@brendan-desktop.(none)>
2012-03-09 11:10:50 -0500

    ELIS-4222 Tweaking settings for version 1 import

--- blocks/rlip/importplugins/version1/settings.php
+++ blocks/rlip/importplugins/version1/settings.php
@@ -1,16 +1,37 @@
 <?php
 
-//setting for "create or update"
-$settings->add(new admin_setting_configcheckbox('rlipimport_version1/createorupdate',
-                                                get_string('createorupdate', 'rlipimport_version1'),
-                                                get_string('configcreateorupdate', 'rlipimport_version1'), 0));
-
-//log file location
-$settings->add(new admin_setting_configtext('rlipimport_version1/logfilelocation',
-                                            get_string('logfilelocation', 'rlipimport_version1'),
-                                            get_string('configlogfilelocation', 'rlipimport_version1'), ''));
+//start of "data handling" section
+$settings->add(new admin_setting_heading('rlipimport_version1/datahandling',
+                                         get_string('datahandling', 'rlipimport_version1'),
+                                         ''));
 
 //groups / groupings
 $settings->add(new admin_setting_configcheckbox('rlipimport_version1/creategroupsandgroupings',
                                                 get_string('creategroupsandgroupings', 'rlipimport_version1'),
                                                 get_string('configcreategroupsandgroupings', 'rlipimport_version1'), ''));
+
+//setting for "create or update"
+$settings->add(new admin_setting_configcheckbox('rlipimport_version1/createorupdate',
+                                                get_string('createorupdate', 'rlipimport_version1'),
+                                                get_string('configcreateorupdate', 'rlipimport_version1'), 0));
+
+//start of "scheduling" section
+$settings->add(new admin_setting_heading('rlipimport_version1/scheduling',
+                                         get_string('scheduling', 'rlipimport_version1'),
+                                         ''));
+
+//setting for disabling in Moodle cron
+//todo: actually use this setting's value somewhere
+$settings->add(new admin_setting_configcheckbox('rlipimport_version1/disableincron',
+                                                get_string('disableincron', 'rlipimport_version1'),
+                                                get_string('configdisableincron', 'rlipimport_version1'), ''));
+
+//start of "logging" section
+$settings->add(new admin_setting_heading('rlipimport_version1/logging',
+                                         get_string('logging', 'rlipimport_version1'),
+                                         ''));
+
+//log file location
+$settings->add(new admin_setting_configtext('rlipimport_version1/logfilelocation',
+                                            get_string('logfilelocation', 'rlipimport_version1'),
+                                            get_string('configlogfilelocation', 'rlipimport_version1'), ''));
