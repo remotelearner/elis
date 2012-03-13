dc07ac91e7de4c359b55d1696319cebe43a7eb45
Justin Filip <jfilip@remote-learner.ca>
2012-03-13 16:31:05 -0400

    ELIS-4738 Created a new test file and wrote a single test for handling verification that creating a new Program and saving it to the DB works correctly.

---
+++ elis/program/phpunit/test_element_creation.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Test creation of PM elements. *
+ * ELIS(TM): Enterprise Learning Intelligence Suite
+ * Copyright (C) 2008-2011 Remote-Learner.net Inc (http://www.remote-learner.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    elis
+ * @subpackage program
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ *
+ */
+
+require_once(dirname(__FILE__) . '/../../core/test_config.php');
+global $CFG;
+require_once($CFG->dirroot . '/elis/program/lib/setup.php');
+require_once(elis::lib('testlib.php'));
+require_once('PHPUnit/Extensions/Database/DataSet/CsvDataSet.php');
+require_once(elispm::lib('data/curriculum.class.php'));
+require_once(elispm::lib('data/track.class.php'));
+require_once(elispm::lib('data/course.class.php'));
+require_once(elispm::lib('data/pmclass.class.php'));
+require_once(elispm::lib('data/user.class.php'));
+require_once(elispm::lib('data/userset.class.php'));
+
+class test_element_creation extends elis_database_test {
+    protected $backupGlobalsBlacklist = array('DB');
+
+    protected static function get_overlay_tables() {
+        return array(
+            'context'         => 'moodle',
+            'course'          => 'moodle',
+            curriculum::TABLE => 'elis_program',
+            track::TABLE      => 'elis_program',
+            course::TABLE     => 'elis_program',
+            pmclass::TABLE    => 'elis_program',
+            user::TABLE       => 'elis_program',
+            userset::TABLE    => 'elis_program',
+        );
+    }
+
+    protected static function get_ignored_tables() {
+        return array(
+            // these aren't actually used, but userset::delete will run a query
+            // on them
+//             clustercurriculum::TABLE => 'elis_program',
+//             clustertrack::TABLE => 'elis_program',
+//             clusterassignment::TABLE => 'elis_program',
+//             userset_profile::TABLE => 'elis_program',
+//             field_data_int::TABLE => 'elis_core',
+//             field_data_num::TABLE => 'elis_core',
+//             field_data_char::TABLE => 'elis_core',
+//             field_data_text::TABLE => 'elis_core',
+//             'block_instances' => 'moodle',
+//             'block_positions' => 'moodle',
+//             'filter_active' => 'moodle',
+//             'filter_config' => 'moodle',
+//             'comments' => 'moodle',
+//             'rating' => 'moodle',
+//             'cache_flags' => 'moodle',
+//             'role_assignments' => 'moodle',
+//             'role_capabilities' => 'moodle',
+//             'role_names' => 'moodle',
+        );
+    }
+
+//     protected function setUp() {
+//         parent::setUp();
+//         $this->setUpContextsTable();
+//     }
+
+    /**
+     * Set up the contexts table with the minimum that we need.
+     */
+    private function setUpContextsTable() {
+        $syscontext = self::$origdb->get_record('context', array('contextlevel' => CONTEXT_SYSTEM));
+        self::$overlaydb->import_record('context', $syscontext);
+
+        $site = self::$origdb->get_record('course', array('id' => SITEID));
+        self::$overlaydb->import_record('course', $site);
+        $sitecontext = self::$origdb->get_record('context', array('contextlevel' => CONTEXT_COURSE,
+                                                                  'instanceid' => SITEID));
+        self::$overlaydb->import_record('context', $sitecontext);
+    }
+
+    /**
+     * Test that a new Program instance can be created and saved to the database.
+     */
+    public function testCreateProgram() {
+        // Create a new test program and save it to the database
+        $pdata = array(
+            'idnumber' => 'TESTID001',
+            'name'     => 'Test Program 1'
+        );
+
+        $newprogram = new curriculum($pdata);
+        $newprogram->save();
+
+        // Verify that the object was saved to the database and the record ID was assigned to the object
+        $this->assertGreaterThan(0, $newprogram->id);
+
+        // Fetch the record from the database
+        $testprogram = new curriculum($newprogram->id);
+
+        // Verify that the
+        $this->assertEquals($newprogram->idnumber, $testprogram->idnumber);
+        $this->assertEquals($newprogram->name, $testprogram->name);
+    }
+}
