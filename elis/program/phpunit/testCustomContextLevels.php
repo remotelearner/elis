b12cf2e6e54f2fb727b8d5800bf7377389c3ab30
Justin Filip <jfilip@remote-learner.ca>
2012-03-16 11:21:07 -0400

    ELIS-4791 Wrote the remaining unit tests for the custom context classes.

--- elis/program/phpunit/testCustomContextLevels.php
+++ elis/program/phpunit/testCustomContextLevels.php
@@ -184,7 +184,7 @@ class test_element_creation extends elis_database_test {
     }
 
     /**
-     * Test that a new Program instance can be created and saved to the database.
+     * Test that a new Program context instance can be created and saved to the database.
      */
     public function testProgramContext() {
         $newobj = $this->initProgram();
@@ -199,6 +199,148 @@ class test_element_creation extends elis_database_test {
 
         // Create the context path to validate this in the returned context object
         $path = '/'.SYSCONTEXTID.'/'.$context->id;
+
+        $this->assertEquals($path, $context->path);
+        $this->assertEquals(substr_count($path, '/'), $context->depth);
+    }
+
+    /**
+     * Test that a new Track context instance can be created and saved to the database.
+     */
+    public function testTrackContext() {
+        $program = $this->initProgram();
+        $program->save();
+
+        $newobj = $this->initTrack($program->id);
+
+        $context = context_elis_track::instance($newobj->id);
+
+        // Validate that a context record was actually created with correct values
+        $this->assertGreaterThan(0, $context->id);
+        $this->assertEquals(CONTEXT_ELIS_TRACK, $context->contextlevel);
+        $this->assertEquals($newobj->id, $context->instanceid);
+
+        // Create the context path to validate this in the returned context object
+        $pctx = context_elis_program::instance($program->id);
+        $path = '/'.SYSCONTEXTID.'/'.$pctx->id.'/'.$context->id;
+
+        $this->assertEquals($path, $context->path);
+        $this->assertEquals(substr_count($path, '/'), $context->depth);
+    }
+
+    /**
+     * Test that a new Course context instance can be created and saved to the database.
+     */
+    public function testCourseContext() {
+        $newobj = $this->initCourse();
+        $newobj->save();
+
+        $context = context_elis_course::instance($newobj->id);
+
+        // Validate that a context record was actually created with correct values
+        $this->assertGreaterThan(0, $context->id);
+        $this->assertEquals(CONTEXT_ELIS_COURSE, $context->contextlevel);
+        $this->assertEquals($newobj->id, $context->instanceid);
+
+        // Create the context path to validate this in the returned context object
+        $path = '/'.SYSCONTEXTID.'/'.$context->id;
+
+        $this->assertEquals($path, $context->path);
+        $this->assertEquals(substr_count($path, '/'), $context->depth);
+    }
+
+    /**
+     * Test that a new Class context instance can be created and saved to the database.
+     */
+    public function testClassContext() {
+        $course = $this->initCourse();
+        $course->save();
+
+        $newobj = $this->initClass($course->id);
+
+        $context = context_elis_class::instance($newobj->id);
+
+        // Validate that a context record was actually created with correct values
+        $this->assertGreaterThan(0, $context->id);
+        $this->assertEquals(CONTEXT_ELIS_CLASS, $context->contextlevel);
+        $this->assertEquals($newobj->id, $context->instanceid);
+
+        // Create the context path to validate this in the returned context object
+        $cctx = context_elis_course::instance($course->id);
+        $path = '/'.SYSCONTEXTID.'/'.$cctx->id.'/'.$context->id;
+
+        $this->assertEquals($path, $context->path);
+        $this->assertEquals(substr_count($path, '/'), $context->depth);
+    }
+
+    /**
+     * Test that a new User context instance can be created and saved to the database.
+     */
+    public function testUserContext() {
+        $newobj = $this->initUser();
+        $newobj->save();
+
+        $context = context_elis_user::instance($newobj->id);
+
+        // Validate that a context record was actually created with correct values
+        $this->assertGreaterThan(0, $context->id);
+        $this->assertEquals(CONTEXT_ELIS_USER, $context->contextlevel);
+        $this->assertEquals($newobj->id, $context->instanceid);
+
+        // Create the context path to validate this in the returned context object
+        $path = '/'.SYSCONTEXTID.'/'.$context->id;
+
+        $this->assertEquals($path, $context->path);
+        $this->assertEquals(substr_count($path, '/'), $context->depth);
+    }
+
+    /**
+     * Test that a new parent User Set context instance can be created and saved to the database.
+     */
+    public function testParentUsersetContext() {
+        $newobj = $this->initUserset();
+        $newobj->save();
+
+        $context = context_elis_userset::instance($newobj->id);
+
+        // Validate that a context record was actually created with correct values
+        $this->assertGreaterThan(0, $context->id);
+        $this->assertEquals(CONTEXT_ELIS_USER, $context->contextlevel);
+        $this->assertEquals($newobj->id, $context->instanceid);
+
+        // Create the context path to validate this in the returned context object
+        $path = '/'.SYSCONTEXTID.'/'.$context->id;
+
+        $this->assertEquals($path, $context->path);
+        $this->assertEquals(substr_count($path, '/'), $context->depth);
+    }
+
+    /**
+     * Test that a new parent User Set context instance can be created and saved to the database.
+     */
+    public function testChildUsersetContext() {
+        $newobj = $this->initUserset();
+        $newobj->save();
+
+        $ctx1 = context_elis_userset::instance($newobj->id);
+
+        $data = array(
+            'name'    => 'Test Sub User Set 1A',
+            'display' => 'We\'re just testing user set creation with child user sets!'
+        );
+
+        $subuserset = new userset($data);
+        $subuserset->save();
+
+        $context = context_elis_userset::instace($subuserset->id);
+
+        // Validate that a context record was actually created with correct values
+        $this->assertGreaterThan(0, $context->id);
+        $this->assertEquals(CONTEXT_ELIS_USER, $context->contextlevel);
+        $this->assertEquals($newobj->id, $context->instanceid);
+
+        // Create the context path to validate this in the returned context object
+        $path = '/'.SYSCONTEXTID.'/'.$ctx1->id.'/'.$context->id;
 
         $this->assertEquals($path, $context->path);
         $this->assertEquals(substr_count($path, '/'), $context->depth);
