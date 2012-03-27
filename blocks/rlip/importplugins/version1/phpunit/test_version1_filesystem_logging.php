6f8e016a51888eb9d13ba30e2b83dc7199a53acb
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-03-27 10:28:30 -0400

    ELIS-4931: Change log message to reflex that invalid country code maybe country, and in PHPunit tests.

--- blocks/rlip/importplugins/version1/phpunit/test_version1_filesystem_logging.php
+++ blocks/rlip/importplugins/version1/phpunit/test_version1_filesystem_logging.php
@@ -3694,7 +3694,7 @@ class version1FilesystemLoggingTest extends elis_database_test {
                       'lang' => 'en',
                       'country' => 'invalidcountry'
                      );
-        $expected_error = "[user.csv line 2] \"country\" value of invalidcountry is not a valid country code.\n";
+        $expected_error = "[user.csv line 2] \"country\" value of invalidcountry is not a valid country or country code.\n";
         $this->assert_data_produces_error($data, $expected_error, 'user');
     }
 
