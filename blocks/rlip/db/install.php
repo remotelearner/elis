2fe7d3510f232d00b01a8b0604003c19cf60c78f
Justin Filip <jfilip@remote-learner.ca>
2012-04-12 17:19:56 -0400

    ELIS-5142 Fixed a problem that was preventing the installation from finishing correctly.

--- blocks/rlip/db/install.php
+++ blocks/rlip/db/install.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__).'../lib.php');
+require_once(dirname(__FILE__).'/../lib.php');
 
 function xmldb_block_rlip_install() {
     // Ensure that scheduling is setup correctly
