4201b0bf116f8901384c271f7c22ef74fd8231e2
Linda Vanderbaan <linda.vanderbaan@remote-learner.net>
2012-04-24 10:25:11 -0400

    ELIS-5233 added upgrade step to handle renaming the table and fields in elis_files_organization_store to elis_userset_store etc

--- repository/elis_files/db/upgrade.php
+++ repository/elis_files/db/upgrade.php
@@ -50,6 +50,42 @@ function xmldb_repository_elis_files_upgrade($oldversion = 0) {
         if (!$errors) {
             set_config('initialized', 1, ELIS_files::$plugin_name);
         }
+
+        // elis_files savepoint reached
+        upgrade_plugin_savepoint(true, 2011110301, 'repository', 'elis_files');
+    }
+
+    if ($oldversion < 2012042300) {
+        //check that elis_files_organization_store exists and elis_files_userset_store does not exist
+        $dbman = $DB->get_manager();
+        if ($dbman->table_exists('elis_files_organization_store') && !$dbman->table_exists('elis_files_userset_store')) {
+
+            $original_table = new xmldb_table('elis_files_organization_store');
+
+            //rename table
+            $dbman->rename_table($original_table,'elis_files_userset_store');
+
+            //drop the keys
+            $original_uuid_index = new xmldb_index('elisfileorgastor_orguu_uix', XMLDB_INDEX_UNIQUE, array('organizationid', 'uuid'));
+            $original_index = new xmldb_index('elisfileorgastor_org_ix', XMLDB_INDEX_NOTUNIQUE, array('organizationid'));
+            $dbman->drop_index($original_table, $original_uuid_index);
+            $dbman->drop_index($original_table, $original_index);
+
+            $new_table = new xmldb_table('elis_userset_store');
+
+            //rename fields
+            $organization = new xmldb_field('organizationid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, '0');
+            $dbman->rename_field($new_table, $organization, 'usersetid');
+
+            //add the keys
+            $new_uuid_index = new xmldb_index('elisfileuserstor_useuu_uix', XMLDB_INDEX_UNIQUE, array('usersetid', 'uuid'));
+            $new_index = new xmldb_index('elisfileuserstor_use_ix', XMLDB_INDEX_NOTUNIQUE, array('usersetid'));
+            $dbman->add_index($new_table, $new_uuid_index);
+            $dbman->add_index($new_table, $new_index);
+        }
+
+        // elis_files savepoint reached
+        upgrade_plugin_savepoint(true, 2012042300, 'repository', 'elis_files');
     }
 
     return $result;
