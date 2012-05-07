f52edce7adc9d49ee3ae71253db208855865553a
Justin Filip <jfilip@remote-learner.ca>
2012-05-07 08:49:09 -0400

    ELIS-5456 Bumpbed build date and changed PRERELEASE to INTEGRATION in the release information for elis/core.

--- elis/program/version.php
+++ elis/program/version.php
@@ -3,4 +3,4 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2012050300;
-$plugin->release = '2.1.0.x-PRERELEASE (Build: 20120420)';
+$plugin->release = '2.1.0.x-INTEGRATION (Build: 20120504)';
