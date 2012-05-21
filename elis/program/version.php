e9e5581abb8db16a9b86789f29671d653f7d2dc9
Justin Filip <jfilip@remote-learner.ca>
2012-05-21 10:15:12 -0400

    ELIS-5734 Bumped build date.

--- elis/program/version.php
+++ elis/program/version.php
@@ -3,4 +3,4 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2012051500;
-$plugin->release = '2.1.0.x-INTEGRATION (Build: 20120504)';
+$plugin->release = '2.1.0.x-INTEGRATION (Build: 20120518)';
