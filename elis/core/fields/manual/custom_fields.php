473df11cfc99377ba832389706b11c7aa6b2cc69
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-06-06 11:21:22 -0400

    ELIS-3902,ELIS-5933: Don't display custom field category if no editable or viewable fields within.

--- elis/core/fields/manual/custom_fields.php
+++ elis/core/fields/manual/custom_fields.php
@@ -26,6 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+// Defines for return codes from manual_field_is_view_or_editable()
+define('MANUAL_FIELD_NO_VIEW_OR_EDIT', -1);
+define('MANUAL_FIELD_VIEWABLE', 0);
+define('MANUAL_FIELD_EDITABLE', 1);
+
 // Form functions
 
 /**
@@ -208,23 +213,24 @@ function manual_field_save_form_data($form, $field, $data) {
     }
 }
 
+
 /**
- * Add an element to a form for a field.
+ * Check whether a field has view or edit capability on context(s)
  *
- * @param object $form the moodle form object we are adding the element to
- * @param object $mform the moodle quick form object belonging to the moodle form
- * @param array $customdata any additional information to pass along to the element
  * @param object $field the custom field we are viewing / editing
- * @param boolean $check_required if true, add a required rule for this field
+ * @param mixed $context
  * @param string $context_edit_cap the edit capability to check if the field owner
  *                                 is set up to use the "edit this context" option for editing
  * @param string $context_view_cap the view capability to check if the field owner
  *                                 is set up to use the "view this context" option for viewing
+ * @return int   MANUAL_FIELD_NO_VIEW_OR_EDIT (-1) if not viewable or editable
+ *               MANUAL_FIELD_VIEWABLE (0) if viewable
+ *               MANUAL_FIELD_EDITABLE (1) if editable (which implies viewable)
  */
-function manual_field_add_form_element($form, $mform, $context, $customdata, $field, $check_required = true,
-                                       $context_edit_cap = NULL, $context_view_cap = NULL) {
-    //$mform = $form->_form;
-    $elem = "field_{$field->shortname}";
+function manual_field_is_view_or_editable($field, $context, $context_edit_cap = NULL, $context_view_cap = NULL) {
+    if (!isset($field->owners['manual'])) {
+        return MANUAL_FIELD_NO_VIEW_OR_EDIT;
+    }
     $manual = new field_owner($field->owners['manual']);
 
     //determine which exact capabilities we are checking
@@ -242,21 +248,53 @@ function manual_field_add_form_element($form, $mform, $context, $customdata, $fi
 
     if ($edit_cap == NULL || $view_cap == NULL) {
         //capabilities for editing or viewing the context were not correctly specified
-        return;
+        return MANUAL_FIELD_NO_VIEW_OR_EDIT;
     }
 
     if ($edit_cap == 'disabled' || !has_capability($edit_cap, $context)) {
-
         if (!has_capability($view_cap, $context)) {
-            //do not have view or edit permissions, so show nothing
-            return;
+            //do not have view or edit permissions
+            return MANUAL_FIELD_NO_VIEW_OR_EDIT;
         }
+        return MANUAL_FIELD_VIEWABLE;
+    }
 
-        //have view but not edit, show as static
-        $mform->addElement('static', $elem, $field->name);
+    return MANUAL_FIELD_EDITABLE;
+}
+
+/**
+ * Add an element to a form for a field.
+ *
+ * @param object $form the moodle form object we are adding the element to
+ * @param object $mform the moodle quick form object belonging to the moodle form
+ * @param mixed $context
+ * @param array $customdata any additional information to pass along to the element
+ * @param object $field the custom field we are viewing / editing
+ * @param boolean $check_required if true, add a required rule for this field
+ * @param string $context_edit_cap the edit capability to check if the field owner
+ *                                 is set up to use the "edit this context" option for editing
+ * @param string $context_view_cap the view capability to check if the field owner
+ *                                 is set up to use the "view this context" option for viewing
+ */
+function manual_field_add_form_element($form, $mform, $context, $customdata, $field, $check_required = true,
+                                       $context_edit_cap = NULL, $context_view_cap = NULL) {
+    //$mform = $form->_form;
+
+    $is_view_or_editable = manual_field_is_view_or_editable($field, $context, $context_edit_cap, $context_view_cap);
+
+    if ($is_view_or_editable == MANUAL_FIELD_NO_VIEW_OR_EDIT) {
         return;
     }
 
+    $elem = "field_{$field->shortname}";
+    if ($is_view_or_editable == MANUAL_FIELD_VIEWABLE) {
+        //have view but not edit, show as static
+        $mform->addElement('static', $elem, $field->name);
+        // TBD: help link?
+        return;
+    }
+
+    $manual = new field_owner($field->owners['manual']);
     $control = $manual->param_control;
     require_once elis::plugin_file('elisfields_manual',"field_controls/{$control}.php");
     call_user_func("{$control}_control_display", $form, $mform, $customdata, $field);
