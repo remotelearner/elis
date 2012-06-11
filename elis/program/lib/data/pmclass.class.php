9ad74dae28e23106f7277b32cc82a8eb84fa32a5
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-06-11 11:36:26 -0400

    ELIS-5509,ELIS-5960: Re-factor appned_once() to use options array() instead of positional params, and pass maxlength.

--- elis/program/lib/data/pmclass.class.php
+++ elis/program/lib/data/pmclass.class.php
@@ -835,12 +835,8 @@ class pmclass extends data_object_with_custom_fields {
         }
         $idnumber = $clone->idnumber;
         if (isset($userset)) {
-            $to_append = ' - '. $userset->name;
-            // if cluster specified, append cluster's name to class
-            if (stripos($idnumber, $to_append) === FALSE) {
-                $maxlen = 92 - strlen($userset->name);
-                $idnumber = append_once(substr($idnumber, 0, $maxlen), $to_append);
-            }
+            $idnumber = append_once($idnumber, ' - '. $userset->name,
+                                    array('maxlength' => 95));
         }
 
         //get a unique idnumber
