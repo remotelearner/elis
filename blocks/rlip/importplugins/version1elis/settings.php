dc7f0678216aef1eb7398c3a720fcf93ed3a07f8
brendan <brendan@brendan-desktop.(none)>
2012-06-12 10:50:14 -0400

    ELIS-6393 Basis for ELIS plugins and ability to view them in the UI

---
+++ blocks/rlip/importplugins/version1elis/settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$settings->add(new admin_setting_heading('placeholder', get_string('placeholdersetting', 'rlipimport_version1elis'),
+                                         get_string('configplaceholdersetting', 'rlipimport_version1elis')));
