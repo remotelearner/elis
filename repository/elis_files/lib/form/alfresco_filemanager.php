d916ccb8fabb85b416e059329f15f16fe7d31984
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-06-15 16:31:41 -0400

    ELIS-5984,ELIS-6423: Create alfresco_filemanager formslib element based off /lib/form/filemanager.php

---
+++ repository/elis_files/lib/form/alfresco_filemanager.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * ELIS(TM): Enterprise Learning Intelligence Suite
+ * Copyright (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    repository
+ * @subpackage elis_files
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once("{$CFG->libdir}/form/filemanager.php");
+
+class alfresco_filemanager extends MoodleQuickForm_filemanager {
+    /**
+     * the name used to identify the formslib element
+     */
+    const NAME = 'alfresco_filemanager';
+
+    /**
+     * Construct a alfresco_filemanager.
+     *
+     * @param string $elementName Element's name
+     * @param mixed $elementLabel Label(s) for an element
+     * @param array $options Options to control the element's display
+     * @param mixed $attributes Either a typical HTML attribute string or an associative array
+     */
+    public function alfresco_filemanager($elementName=null, $elementLabel=null, $attributes=null, $options=null) {
+        parent::__construct($elementName, $elementLabel, $attributes, $options);
+    }
+
+}
+
+/* first argument is the string that will be used to identify the element.
+ * second argument is the filename that contains the class definition
+ * third argument is the class name
+ */
+MoodleQuickForm::registerElementType(alfresco_filemanager::NAME, __FILE__, 'alfresco_filemanager');
+
