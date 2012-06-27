4caaf68d20b06a512dea5af275dbbce4c347f875
brendan <brendan@brendan-desktop.(none)>
2012-06-27 13:40:34 -0400

    ELIS-6243 Unit test for validating that track enrolments create program enrolments

---
+++ blocks/rlip/importplugins/version1elis/phpunit/test_elis_enrolment_side_effects.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * ELIS(TM): Enterprise Learning Intelligence Suite
+ * Copyright (C) 2008-2012 Remote-Learner.net Inc (http://www.remote-learner.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    rlip
+ * @subpackage importplugins/version1elis/phpunit
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ *
+ */
+
+if (!isset($_SERVER['HTTP_USER_AGENT'])) {
+    define('CLI_SCRIPT', true);
+}
+
+require_once(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))).'/config.php');
+require_once(dirname(__FILE__) .'/rlip_mock_provider.class.php');
+global $CFG;
+require_once($CFG->dirroot.'/elis/core/lib/testlib.php');
+
+/**
+ * Class for validating side effects of PM enrolments
+ */
+class elis_enrolment_side_effects_test extends elis_database_test {
+    /**
+     * Return the list of tables that should be overlayed.
+     */
+    static protected function get_overlay_tables() {
+        global $CFG;
+        require_once($CFG->dirroot.'/elis/program/lib/setup.php');
+        require_once(elispm::lib('data/curriculum.class.php'));
+        require_once(elispm::lib('data/curriculumstudent.class.php'));
+        require_once(elispm::lib('data/track.class.php'));
+        require_once(elispm::lib('data/user.class.php'));
+        require_once(elispm::lib('data/usertrack.class.php'));
+
+        return array(curriculum::TABLE => 'elis_program',
+                     curriculumstudent::TABLE => 'elis_program',
+                     track::TABLE => 'elis_program',
+                     user::TABLE => 'elis_program',
+                     usertrack::TABLE => 'elis_program');
+    }
+
+    /**
+     * Return the list of tables that should be ignored for writes.
+     */
+    static protected function get_ignored_tables() {
+        global $CFG;
+        require_once($CFG->dirroot.'/elis/program/lib/setup.php');
+        require_once(elispm::lib('data/usermoodle.class.php'));
+
+        return array('context' => 'moodle',
+                     'user' => 'moodle',
+                     usermoodle::TABLE => 'moodle');
+    }
+
+    /**
+     * Validate that enrolling a user into a track via IP auto-enrolls them in the
+     * appropriate program
+     */
+    public function test_track_enrolment_creates_program_enrolment() {
+        global $CFG, $DB;
+        require_once($CFG->dirroot.'/elis/program/lib/setup.php');
+        require_once(elispm::lib('data/curriculum.class.php'));
+        require_once(elispm::lib('data/curriculumstudent.class.php'));
+        require_once(elispm::lib('data/track.class.php'));
+        require_once(elispm::lib('data/user.class.php'));
+
+        //set up data
+        $user = new user(array('idnumber' => 'testuseridnumber',
+                               'username' => 'testuserusername',
+                               'firstname' => 'testuserfirstname',
+                               'firstname' => 'testuserfirstname',
+                               'lastname' => 'testuserlastname',
+                               'email' => 'test@useremail.com',
+                               'country' => 'CA'));
+        $user->save();
+
+        $program = new curriculum(array('idnumber' => 'testprogramidnumber'));
+        $program->save();
+
+        $track = new track(array('curid' => $program->id,
+                                 'idnumber' => 'testtrackidnumber'));
+        $track->save();
+
+        //run the enrolment create action
+        $record = new stdClass;
+        $record->context = 'track_testtrackidnumber';
+        $record->user_username = 'testuserusername';
+
+        $importplugin = rlip_dataplugin_factory::factory('rlipimport_version1elis');
+        $importplugin->track_enrolment_create($record, 'bogus', 'testtrackidnumber');
+
+        //validation
+        $this->assertTrue($DB->record_exists(curriculumstudent::TABLE, array('userid' => $user->id,
+                                                                             'curriculumid' => $program->id)));
+    }
+}
