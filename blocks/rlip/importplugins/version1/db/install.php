0c0dfe894f0f079ca5b1d69f59af2f7a5db8dccd
brendan <brendan@brendan-desktop.(none)>
2012-07-27 10:48:59 -0400

    ELIS-6252 Got the upgrade from 1.9 ELIS IP to 2.x ELIS IP working

--- blocks/rlip/importplugins/version1/db/install.php
+++ blocks/rlip/importplugins/version1/db/install.php
@@ -30,13 +30,21 @@ require_once($CFG->dirroot.'/blocks/rlip/lib.php');
 require_once($CFG->dirroot.'/blocks/rlip/importplugins/version1/version1.class.php');
 
 function xmldb_rlipimport_version1_install() {
-
-    global $DB;
+    global $CFG, $DB;
 
     $dbman = $DB->get_manager();
     if (!$dbman->table_exists('block_rlip_fieldmap')) {
         //this is a fresh install with no previous 1.9-version IP set up
         //so no need to migrate data
+        return true;
+    }
+
+    // Skip this work if we should be using ELIS IP
+    $pm_installed = file_exists($CFG->dirroot.'/elis/program/lib/setup.php');
+    $ip_basic_enabled = !empty($CFG->block_rlip_overrideelisip);
+    $init_moodle_plugins = !$pm_installed || $ip_basic_enabled;
+
+    if (!$init_moodle_plugins) {
         return true;
     }
 
