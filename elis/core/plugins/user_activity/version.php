b6cc2d5225e4a72e694d7ff58c5192060de335f6
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-11-02 15:47:33 -0400

    ELIS-7901,ELIS-7907: Added ETL upgrade step to limit etl_user_module_activity duration <= hour (as with etl_user_activity) + PHPUnit test to verify. (cherry picked from commit 570eacb11a488bca296fefd9f78c1fdca7e582bb)
    Conflicts:
    
    	elis/core/plugins/user_activity/version.php

--- elis/core/plugins/user_activity/version.php
+++ elis/core/plugins/user_activity/version.php
@@ -2,5 +2,5 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2012021400;
-$plugin->release = '2.0.2-PRERELEASE (Build: 20120214)';
+$plugin->version = 2012021401;
+$plugin->release = '2.2.0.x-PRERELEASE (Build: 20121114)';
