5f6d70f2f9c21297fb420b70cded6123eb529c51
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-11-16 14:51:47 -0500

    ELIS-7876,ELIS-7877: Add username & idnumber indexes to crlm_user, improve user sync health check (cherry picked from commit 8e9c8386ed18837136b041520cabbfae747dab70)
    Conflicts:
    
    	elis/program/version.php

--- elis/program/version.php
+++ elis/program/version.php
@@ -2,5 +2,5 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2012062900;
-$plugin->release = '2.3.1.x-PRERELEASE (Build: 20121114)';
+$plugin->version = 2012062901;
+$plugin->release = '2.3.1.x-PRERELEASE (Build: 20121115)';
