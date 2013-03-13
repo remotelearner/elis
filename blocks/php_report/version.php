08ed7f94fb2db606202b0445fc982d0a36582e04
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-03-13 10:51:30 -0400

    Update block DB version to current date, release to 2.4.0.1 and build date.

--- blocks/php_report/version.php
+++ blocks/php_report/version.php
@@ -15,5 +15,5 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version = 2011060300;
-$plugin->release = '2.3.1.5 (Build: 20130205)';
+$plugin->version = 2013031300;
+$plugin->release = '2.4.0.1 (Build: 20130313)';
