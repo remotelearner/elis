d5081fc36be123f72041b46f14062af70a6460fa
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-04-11 12:37:29 -0400

    ELIS-8349: IUR single language string with 'curriculum' => 'program'

--- blocks/php_report/instances/individual_user/lang/en/rlreport_individual_user.php
+++ blocks/php_report/instances/individual_user/lang/en/rlreport_individual_user.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * ELIS(TM): Enterprise Learning Intelligence Suite
+ * Copyright (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    block/php_report
+ * @subpackage individual_user
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die();
 
 $string['anyvalue'] = 'Select...';
 $string['column_completion_element'] = 'Learning Objective';
@@ -26,7 +52,7 @@ $string['individual_user_report_help'] = '<p align="center"><b>Individual User R
 
 <p>Please select a user from this pulldown.</p>';
 $string['invalidpermission'] = 'You do not have permission to view this report';
-$string['footer_credits_for'] = '{$a} credits for this curriculum';
+$string['footer_credits_for'] = '{$a} credits for this program';
 $string['footer_credits_of'] = '{$a} credits of a required';
 $string['footer_has_earned'] = '{$a} has earned';
 $string['na'] = 'N/A';
