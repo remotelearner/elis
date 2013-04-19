b7f51cd29434b928b4134a854ce176458bb84270
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-04-19 12:29:31 -0400

    ELIS-7780: Remove old, unused Jasper report capabilities causing confusion
    Conflicts:
    
    	elis/program/version.php

--- elis/program/db/upgrade.php
+++ elis/program/db/upgrade.php
@@ -599,5 +599,16 @@ function xmldb_elis_program_upgrade($oldversion=0) {
         upgrade_plugin_savepoint($result, 2012062901, 'elis', 'program');
     }
 
+    // ELIS-7780: remove deprecated capabilites
+    if ($result && $oldversion < 2013041900) {
+        $capstodelete = array('elis/program:viewgroupreports', 'elis/program:viewreports');
+        list($inorequal, $params) = $DB->get_in_or_equal($capstodelete);
+        $where = "capability $inorequal";
+        $DB->delete_records_select('role_capabilities', $where, $params);
+        $where = "name $inorequal";
+        $DB->delete_records_select('capabilities', $where, $params);
+        upgrade_plugin_savepoint($result, 2013041900, 'elis', 'program');
+    }
+
     return $result;
 }
--- elis/program/version.php
+++ elis/program/version.php
@@ -2,5 +2,5 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013032200;
+$plugin->version = 2013041900;
 $plugin->release = '2.4.1.0 (Build: 20130322)';
