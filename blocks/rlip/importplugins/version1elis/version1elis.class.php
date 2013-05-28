35798c3ff5efc238c770430d7718a410a906aff0
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-05-28 17:04:05 -0400

    RECODEV-51: Improve RLDH version1elis get_userid_from_record() for new WebServices

--- blocks/rlip/importplugins/version1elis/version1elis.class.php
+++ blocks/rlip/importplugins/version1elis/version1elis.class.php
@@ -3095,9 +3095,10 @@ class rlip_importplugin_version1elis extends rlip_importplugin_base {
      *
      * @param object $record One record of import data
      * @param string $filename The import file name, used for logging
+     * @param object $returnparams Optional variable to return back parameters used
      * @return mixed The user id, or false if not found
      */
-    function get_userid_from_record($record, $filename) {
+    function get_userid_from_record($record, $filename, &$returnparams = null) {
         global $CFG, $DB;
         require_once($CFG->dirroot.'/elis/program/lib/setup.php');;
         require_once(elispm::lib('data/user.class.php'));
@@ -3112,8 +3113,10 @@ class rlip_importplugin_version1elis extends rlip_importplugin_base {
         if (isset($record->user_idnumber)) {
             $params['idnumber'] = $record->user_idnumber;
         }
-
-        return $DB->get_field(user::TABLE, 'id', $params);
+        if ($returnparams != null) {
+            $returnparams = $params;
+        }
+        return !empty($params) ? $DB->get_field(user::TABLE, 'id', $params) : false;
     }
 
     /**
