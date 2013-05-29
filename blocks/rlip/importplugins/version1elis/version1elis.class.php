66b34c571476367bef24fdecefaa615b1bbe9671
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-05-29 12:18:58 -0400

    RECODEV-51: fixed get_userid_from_record() check for returnparams

--- blocks/rlip/importplugins/version1elis/version1elis.class.php
+++ blocks/rlip/importplugins/version1elis/version1elis.class.php
@@ -3113,7 +3113,7 @@ class rlip_importplugin_version1elis extends rlip_importplugin_base {
         if (isset($record->user_idnumber)) {
             $params['idnumber'] = $record->user_idnumber;
         }
-        if ($returnparams != null) {
+        if ($returnparams !== null) {
             $returnparams = $params;
         }
         if (empty($params)) {
