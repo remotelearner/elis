7c8b3540758e813c8d90fbb4782a519d17f0d891
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-05-30 11:58:06 -0400

    RECODEV-27: Userset enrolment delete webservice & PHPunit tests

--- blocks/rlip/lang/en/block_rlip.php
+++ blocks/rlip/lang/en/block_rlip.php
@@ -103,3 +103,11 @@ $string['runmanually'] = 'Run manually';
 $string['scheduled'] = 'scheduled';
 $string['schedulepagetitle'] = 'Schedule DH';
 $string['schedulingtime'] = "*Scheduled jobs may not run exactly at the specified time.";
+
+$string['ws_unknown_error_code'] = 'unknown_error';
+$string['ws_unknown_error_msg'] = 'An unknown error occurred';
+$string['ws_userset_enrolment_delete_success_code'] = 'userset_enrolment_deleted';
+$string['ws_userset_enrolment_delete_success_msg'] = 'User successfully unenroled from Userset';
+$string['ws_userset_enrolment_delete_fail'] = 'Could not unenrol user from userset';
+$string['ws_userset_enrolment_delete_fail_invalid_userset'] = 'Userset identified by userset_name \'{$a->userset_name}\' is not a valid userset.';
+$string['ws_userset_enrolment_delete_fail_invalid_user'] = 'No unique user identified by {$a->userparams} was found.';
