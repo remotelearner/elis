ef883597866d430f1c22b9857989fc6849b7ec5e
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-06-05 16:43:52 -0400

    RECODEV-35: Program update webservice & PHPUnit tests

--- blocks/rlip/db/services.php
+++ blocks/rlip/db/services.php
@@ -99,7 +99,14 @@ $functions = array(
         'classpath' => 'blocks/rlip/ws/elis/userset_enrolment_delete.class.php',
         'description' => 'Remove a user from a userset',
         'type' => 'read'
-    )
+    ),
+    'block_rldh_elis_program_update' => array(
+        'classname' => 'block_rldh_elis_program_update',
+        'methodname' => 'program_update',
+        'classpath' => 'blocks/rlip/ws/elis/program_update.class.php',
+        'description' => 'Update a program',
+        'type' => 'read'
+     )
 );
 
 // Pre-built service.
@@ -116,7 +123,8 @@ $services = array(
                 'block_rldh_elis_class_enrolment_create',
                 'block_rldh_elis_class_enrolment_delete',
                 'block_rldh_elis_userset_enrolment_create',
-                'block_rldh_elis_userset_enrolment_delete'
+                'block_rldh_elis_userset_enrolment_delete',
+                'block_rldh_elis_program_update'
         ),
         'restrictedusers' => 0,
         'enabled' => 1,
