86173d2cbf25f6f042f3f64bbd800bad4baf5ba3
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-06-06 18:37:22 -0400

    RECODEV-37: Track create webservice & PHPUnit tests

--- blocks/rlip/db/services.php
+++ blocks/rlip/db/services.php
@@ -99,7 +99,14 @@ $functions = array(
         'classpath' => 'blocks/rlip/ws/elis/userset_enrolment_delete.class.php',
         'description' => 'Remove a user from a userset',
         'type' => 'read'
-    )
+    ),
+    'block_rldh_elis_track_create' => array(
+        'classname' => 'block_rldh_elis_track_create',
+        'methodname' => 'track_create',
+        'classpath' => 'blocks/rlip/ws/elis/track_create.class.php',
+        'description' => 'Create a track',
+        'type' => 'read'
+     )
 );
 
 // Pre-built service.
@@ -116,7 +123,8 @@ $services = array(
                 'block_rldh_elis_class_enrolment_create',
                 'block_rldh_elis_class_enrolment_delete',
                 'block_rldh_elis_userset_enrolment_create',
-                'block_rldh_elis_userset_enrolment_delete'
+                'block_rldh_elis_userset_enrolment_delete',
+                'block_rldh_elis_track_create'
         ),
         'restrictedusers' => 0,
         'enabled' => 1,
