547205d87310f5738d6fb0d581b603e26dca6f32
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-06-07 12:39:05 -0400

    RECODEV-38: Track update webservice & PHPUnit tests

--- blocks/rlip/db/services.php
+++ blocks/rlip/db/services.php
@@ -99,7 +99,14 @@ $functions = array(
         'classpath' => 'blocks/rlip/ws/elis/userset_enrolment_delete.class.php',
         'description' => 'Remove a user from a userset',
         'type' => 'read'
-    )
+    ),
+    'block_rldh_elis_track_update' => array(
+        'classname' => 'block_rldh_elis_track_update',
+        'methodname' => 'track_update',
+        'classpath' => 'blocks/rlip/ws/elis/track_update.class.php',
+        'description' => 'Update a track',
+        'type' => 'read'
+     )
 );
 
 // Pre-built service.
@@ -116,7 +123,8 @@ $services = array(
                 'block_rldh_elis_class_enrolment_create',
                 'block_rldh_elis_class_enrolment_delete',
                 'block_rldh_elis_userset_enrolment_create',
-                'block_rldh_elis_userset_enrolment_delete'
+                'block_rldh_elis_userset_enrolment_delete',
+                'block_rldh_elis_track_update'
         ),
         'restrictedusers' => 0,
         'enabled' => 1,
