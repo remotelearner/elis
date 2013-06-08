30e40fa061c831a463667abe0dc8ed9f8e92dd6b
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-06-08 01:40:23 -0400

    RECODEV-41: Userset update webservices & PHPUnit tests

--- blocks/rlip/db/services.php
+++ blocks/rlip/db/services.php
@@ -99,7 +99,14 @@ $functions = array(
         'classpath' => 'blocks/rlip/ws/elis/userset_enrolment_delete.class.php',
         'description' => 'Remove a user from a userset',
         'type' => 'read'
-    )
+    ),
+     'block_rldh_elis_userset_update' => array(
+        'classname' => 'block_rldh_elis_userset_update',
+        'methodname' => 'userset_update',
+        'classpath' => 'blocks/rlip/ws/elis/userset_update.class.php',
+        'description' => 'Update a userset',
+        'type' => 'read'
+     )
 );
 
 // Pre-built service.
@@ -116,7 +123,8 @@ $services = array(
                 'block_rldh_elis_class_enrolment_create',
                 'block_rldh_elis_class_enrolment_delete',
                 'block_rldh_elis_userset_enrolment_create',
-                'block_rldh_elis_userset_enrolment_delete'
+                'block_rldh_elis_userset_enrolment_delete',
+                'block_rldh_elis_userset_update'
         ),
         'restrictedusers' => 0,
         'enabled' => 1,
