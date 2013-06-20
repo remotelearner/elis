ef0e3014be6f9f75b52e0b19bbe1ddde1731048d
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-06-20 14:16:11 -0400

    ELIS-8483: Update version release & build date

--- blocks/php_report/version.php
+++ blocks/php_report/version.php
@@ -16,7 +16,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin->version = 2012060301;
-$plugin->release = '2.3.2.9 (Build: 20130522)';
+$plugin->release = '2.3.2.11 (Build: 20130620)';
 $plugin->dependencies = array(
     'elis_core' => 2013022701
 );
