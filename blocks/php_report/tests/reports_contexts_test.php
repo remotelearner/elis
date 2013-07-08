c6f6df9bc9f7774c1ec97639354f75c9593f1a65
Marko Vidberg <marko.vidberg@remote-learner.net>
2013-07-08 14:04:29 -0400

    ELIS-8480 Converted PHP unit tests

--- blocks/php_report/tests/reports_contexts_test.php
+++ blocks/php_report/tests/reports_contexts_test.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2011 Remote-Learner.net Inc (http://www.remote-learner.net)
+ * Copyright (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,64 +16,45 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    elis
- * @subpackage programmanager
+ * @package    blocks-php_report
  * @author     Remote-Learner.net Inc
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  */
-
 
 /**
  * NOTE: This file only tests all ELIS custom context calls made by ELIS reports. It should not be used to verify
  * other report operations or data accuracy.
  */
 
-
-require_once(dirname(__FILE__) . '/../../../elis/core/test_config.php');
+require_once(dirname(__FILE__).'/../../../elis/core/test_config.php');
 global $CFG;
-require_once($CFG->dirroot . '/elis/program/lib/setup.php');
-require_once(elis::lib('testlib.php'));
+require_once($CFG->dirroot.'/elis/program/lib/setup.php');
 require_once(elispm::lib('data/user.class.php'));
 require_once(elispm::lib('data/userset.class.php'));
 require_once(elis::lib('data/customfield.class.php'));
 require_once('PHPUnit/Extensions/Database/DataSet/CsvDataSet.php');
-ini_set('error_reporting',1);
-ini_set('display_errors',1);
 
-class reportsTest extends elis_database_test {
-    protected $backupGlobalsBlacklist = array('DB');
-
-	protected static function get_overlay_tables() {
-		return array(
-            user::TABLE => 'elis_program',
-            'context'      => 'moodle',
-            'course'       => 'moodle',
-            userset::TABLE => 'elis_program',
-            field_category::TABLE => 'elis_core',
-            field_category_contextlevel::TABLE => 'elis_core',
-            field::TABLE => 'elis_core',
-            field_contextlevel::TABLE => 'elis_core',
-            field_data_char::TABLE => 'elis_core',
-            field_data_int::TABLE => 'elis_core',
-        );
-	}
-
+/**
+ * Class to test PHP report contexts.
+ */
+class reports_contexts_testcase extends elis_database_test {
+    /**
+     * Load test data from CSV file.
+     */
     protected function load_userset_csv_data() {
-        $dataset = new PHPUnit_Extensions_Database_DataSet_CsvDataSet();
-        $dataset->addTable(userset::TABLE, elis::component_file('program', 'phpunit/userset.csv'));
-        $dataset = new PHPUnit_Extensions_Database_DataSet_ReplacementDataSet($dataset);
-        $dataset->addSubStrReplacement('\n', "\n");
-        load_phpunit_data_set($dataset, true, self::$overlaydb);
+        $dataset = $this->createCsvDataSet(array(
+            userset::TABLE => elis::component_file('program', 'tests/fixtures/userset.csv')
+        ));
+        $this->loadDataSet($dataset);
     }
 
-    protected function load_orig_site_course_data() {
-        $site = self::$origdb->get_record('course', array('id' => SITEID));
-        self::$overlaydb->import_record('course', $site);
-    }
-
-    public function testCourseUsageSummary() {
+    /**
+     * Test course usage summary report.
+     * @uses $CFG
+     */
+    public function test_course_usage_summary() {
         global $CFG;
         require_once(dirname(__FILE__).'/../instances/course_usage_summary/course_usage_summary_report.class.php');
         $report = new course_usage_summary_report('test_course_summary');
@@ -81,58 +62,69 @@ class reportsTest extends elis_database_test {
         if (!empty($report->checkboxes_filter->options['choices'])) {
             foreach ($report->checkboxes_filter->options['choices'] as $choice => $desc) {
                 $result = $report->get_average_test_score($choice);
-                $resultIsNumeric = is_numeric(substr($result,0,-1));
-                $this->assertTrue($resultIsNumeric);
+                $resultisnumeric = is_numeric(substr($result, 0, -1));
+                $this->assertTrue($resultisnumeric);
             }
         }
     }
 
-    public function testCourseCompletionByCluster() {
+    /**
+     * Test course completion by cluster report.
+     * @uses $CFG
+     */
+    public function test_course_completion_by_cluster() {
         global $CFG;
         $this->load_userset_csv_data();
         require_once(dirname(__FILE__).'/../instances/course_completion_by_cluster/course_completion_by_cluster_report.class.php');
         $report = new course_completion_by_cluster_report('test_course_completion_by_cluster');
 
-        //test context in get_report_sql
+        // Test context in get_report_sql.
         $columns = $report->get_select_columns();
         $sql = $report->get_report_sql($columns);
-        $this->assertArrayHasKey(0,$sql);
+        $this->assertArrayHasKey(0, $sql);
         $this->assertNotEmpty($sql[0]);
-        $this->assertArrayHasKey(1,$sql);
+        $this->assertArrayHasKey(1, $sql);
         $this->assertNotEmpty($sql[1]);
 
-        //test context in transform_grouping_header_label
+        // Test context in transform_grouping_header_label.
         $grouping = new stdClass;
         $grouping->field = 'cluster.id';
         $grouping->label = 'Test';
-        $grouping_current['cluster.id'] = 1;
+        $groupingcurrent['cluster.id'] = 1;
         $datum = new stdClass;
         $datum->cluster = 1;
-        $result = $report->transform_grouping_header_label($grouping_current, $grouping, $datum, php_report::$EXPORT_FORMAT_LABEL);
+        $result = $report->transform_grouping_header_label($groupingcurrent, $grouping, $datum, php_report::$EXPORT_FORMAT_LABEL);
         $this->assertNotEmpty($result);
     }
 
-    public function testCourseProgressSummary() {
+    /**
+     * Test course progress summary report.
+     * @uses $CFG
+     */
+    public function test_course_progress_summary() {
         global $CFG;
         require_once(dirname(__FILE__).'/../instances/course_progress_summary/course_progress_summary_report.class.php');
         $report = new course_progress_summary_report('test_course_progress_summary');
 
-        //test context in get_columns
+        // Test context in get_columns.
         $columns = $report->get_columns();
         $this->assertNotEmpty($columns);
     }
 
-    public function testIndividualCourseProgress() {
+    /**
+     * Test individual course progress report.
+     * @uses $CFG
+     */
+    public function test_individual_course_progress() {
         global $CFG;
-        $this->load_orig_site_course_data();
         require_once(dirname(__FILE__).'/../instances/individual_course_progress/individual_course_progress_report.class.php');
         $report = new individual_course_progress_report('test_individual_course_progress');
 
-        //test context in get_columns
+        // Test context in get_columns.
         $columns = $report->get_columns();
         $this->assertNotEmpty($columns);
 
-        //test context in get_max_test_score_sql
+        // Test context in get_max_test_score_sql.
         $fields = array('_elis_course_pretest', '_elis_course_posttest');
         foreach ($fields as $field) {
             $report->get_max_test_score_sql($field);
