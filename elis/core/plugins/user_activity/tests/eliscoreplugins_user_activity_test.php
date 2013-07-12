77fe9f56db5182d3e3e87922ab1a4cde8e78f572
Justin Filip <jfilip@remote-learner.ca>
2013-07-12 10:49:48 -0400

    ELIS-8479: Converted eliscoreplugins_user_activity test file.

--- elis/core/plugins/user_activity/tests/eliscoreplugins_user_activity_test.php
+++ elis/core/plugins/user_activity/tests/eliscoreplugins_user_activity_test.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ * Copyright (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,49 +16,46 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    elis
- * @subpackage user_activity
+ * @package    eliscoreplugins_user_activity
  * @author     Remote-Learner.net Inc
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
  *
  */
 
-require_once(dirname(__FILE__) .'/../../../test_config.php');
+require_once(dirname(__FILE__).'/../../../test_config.php');
 global $CFG;
-require_once($CFG->dirroot .'/elis/core/lib/setup.php');
-require_once(elis::lib('testlib.php'));
+require_once($CFG->dirroot.'/elis/core/lib/setup.php');
+require_once($CFG->libdir.'/upgradelib.php');
 require_once(elis::plugin_file('eliscoreplugins_user_activity', 'db/upgrade.php'));
-require_once('PHPUnit/Extensions/Database/DataSet/CsvDataSet.php');
-require_once($CFG->libdir .'/upgradelib.php');
 
-class etl_test extends elis_database_test {
-    protected $backupGlobalsBlacklist = array('DB');
-
-    protected static function get_overlay_tables() {
-        return array(
-            'config' => 'moodle',
-            'config_plugins' => 'moodle',
-            'etl_user_activity' => 'eliscoreplugins_user_activity',
-            'etl_user_module_activity' => 'eliscoreplugins_user_activity'
-        );
-    }
-
+/**
+ * Testing the user_activity plugin.
+ *
+ * @group elis_core
+ * @group eliscoreplugins_user_activity
+ */
+class eliscoreplugins_user_activity_testcase extends elis_database_test {
     /**
-     * Test the user_activity upgrade
+     * Test the user_activity upgrade.
      */
-    public function testETLupgrade() {
+    public function test_etl_upgrade() {
         global $DB;
-        $dataset = new PHPUnit_Extensions_Database_DataSet_CsvDataSet();
-        $dataset->addTable('etl_user_activity', elis::plugin_file('eliscoreplugins_user_activity', 'phpunit/etl_user_activity.csv'));
-        $dataset->addTable('etl_user_module_activity', elis::plugin_file('eliscoreplugins_user_activity', 'phpunit/etl_user_module_activity.csv'));
 
-        $overlaydb = self::$overlaydb;
-        load_phpunit_data_set($dataset, true, $overlaydb);
+        $datatables = array(
+            'etl_user_activity' => elis::plugin_file('eliscoreplugins_user_activity', 'tests/fixtures/etl_user_activity.csv'),
+            'etl_user_module_activity' => elis::plugin_file('eliscoreplugins_user_activity', 'tests/fixtures/etl_user_module_activity.csv')
+        );
+
+        $dataset = $this->createCsvDataSet($datatables);
+        $this->loadDataSet($dataset);
+
+        // Ensure we can run our upgrade steps.
+        $DB->set_field('config_plugins', 'value', 2011101700, array('plugin' => 'eliscoreplugins_user_activity'));
 
         xmldb_eliscoreplugins_user_activity_upgrade(2011101800);
 
         $this->assertFalse($DB->record_exists_select('etl_user_activity', 'duration > 3600'));
         $this->assertFalse($DB->record_exists_select('etl_user_module_activity', 'duration > 3600'));
-     }
+    }
 }
