3994bcd76f7635c37a1282cf1a75aa7c78a6208b
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-08-12 15:54:41 -0400

    ELIS-8528: Remove LOs/completionelements when course description deleted

--- elis/program/version.php
+++ elis/program/version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2012123105;
+$plugin->version = 2012123106;
 $plugin->release = '2.3.2.12 (Build: 20130722)';
 $plugin->dependencies = array(
     'elis_core' => 2013022702
