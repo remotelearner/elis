3b45cb6d5ec928670398d36e1255486fba4470d0
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-08-21 15:59:30 -0400

    ELIS-8543: Update version files db version, release, build date & elis_core dependency

--- blocks/php_report/version.php
+++ blocks/php_report/version.php
@@ -15,8 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version = 2013073100;
-$plugin->release = '2.5.0.0 (Build: 20130731)';
+$plugin->version = 2013082100;
+$plugin->release = '2.5.1.0 (Build: 20130821)';
 $plugin->dependencies = array(
-    'elis_core' => 2013051401
+    'elis_core' => 2013082100
 );
