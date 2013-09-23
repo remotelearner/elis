60160881565de1fcc01642b827c5ed6613e57d06
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-09-23 15:18:40 -0400

    ELIS-8569,ELIS-8441: Fix Program defaults of invalid '0y' for timetocomplete & frequency
    Conflicts:
    
    	elis/program/db/upgrade.php
    	elis/program/version.php

--- elis/program/version.php
+++ elis/program/version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013051503;
+$plugin->version = 2013051504;
 $plugin->release = '2.4.3.1 (Build: 20130911)';
 $plugin->dependencies = array(
     'elis_core' => 2013051401
