5e0044c643a272c681db3b0e4c8fab94ddcfab72
Akinsaya Delamarre <adelamarre@remote-learner.net>
2013-09-26 14:57:03 -0400

    ELIS-8574: Added access.php script and increased the DB version

--- blocks/repository/db/access.php
+++ blocks/repository/db/access.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2011 Remote-Learner.net Inc (http://www.remote-learner.net)
+ * Copyright (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,9 +19,21 @@
  * @package    block_repository
  * @author     Remote-Learner.net Inc
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @copyright  (C) 2008-2011 Remote Learner.net Inc http://www.remote-learner.net
- *
+ * @copyright  (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
  */
 
-$plugin->version = 2011110301;
-$plugin->release = '2.4.3.2 (Build: 20130925)';
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'block/repository:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
