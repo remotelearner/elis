25c182fa5fbed2b98af5024c16ad3c16b4424652
Marko Vidberg <marko@vidberg.com>
2013-11-06 15:08:27 -0500

    ELIS-8597 Added error logging to unlinking of temporary import file

--- blocks/rlip/lib/rlip_importplugin.class.php
+++ blocks/rlip/lib/rlip_importplugin.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2012 Remote-Learner.net Inc (http://www.remote-learner.net)
+ * Copyright (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,11 +16,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    elis
- * @subpackage core
+ * @package    block_rlip
  * @author     Remote-Learner.net Inc
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2008-2012 Remote Learner.net Inc http://www.remote-learner.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  */
 
@@ -767,8 +766,12 @@ abstract class rlip_importplugin_base extends rlip_dataplugin {
         $this->dblogger->flush($filename);
 
         if (!$this->manual) {
-            // delete processed import file - TBD: check return
-            $fileplugin->delete();
+            // Delete processed import file.
+            if (!$fileplugin->delete()) {
+                $message = "Error when attempting to delete temporary file '".$filename."'";
+                mtrace($message);
+                $this->fslogger->log_failure($message, 0, $filename);
+            }
         }
 
         return null;
