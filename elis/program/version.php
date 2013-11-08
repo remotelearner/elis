f759e3ca0819edbef4c1c0c4854650c2f462b6bf
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-11-08 16:12:24 -0500

    ELIS-8306: Changes for multi-language support of custom menu fields
    Conflicts:
    
    	elis/program/version.php

--- elis/program/version.php
+++ elis/program/version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2012123111;
+$plugin->version = 2012123112;
 $plugin->release = '2.3.3.4 (Build: 20131104)';
 $plugin->dependencies = array(
-    'elis_core' => 2013022704
+    'elis_core' => 2013022705
 );
