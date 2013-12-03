6fb84f3fad0ed6a9cb87a173240c991dae618b83
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-12-03 11:28:54 -0500

    ELIS-8639: Update version releases & build dates

--- repository/elis_files/version.php
+++ repository/elis_files/version.php
@@ -18,7 +18,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013082100;
-$plugin->release = '2.5.1.4 (Build: 20131104)';
+$plugin->release = '2.5.1.5 (Build: 20131203)';
 $plugin->dependencies = array(
     'elis_core' => 2013082100
 );
